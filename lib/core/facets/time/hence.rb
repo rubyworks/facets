6713f9ed3ac045ce7ab26f336b375167f09fbed1
Tyler Rick <github.com@tylerrick.com>
2008-11-13 12:00:13 -0800

    Fixed some bugs with Time#ago/Time#hence not changing years correctly when you changed months. (For example, Time.utc(2008, 1, 1).ago(12, :months) incorrectly returned 2009-01-01 instead of 2007-01-01.) Added a bunch of tests for Time#ago/Time#hence. Changed Time#ago/Time#hence to still work if passed negative number.

--- lib/core/facets/time/hence.rb
+++ lib/core/facets/time/hence.rb
@@ -13,15 +13,16 @@ class Time
     # a number of time-units ago.
     #
     def ago(number, units=:seconds)
-      time =(
+      return hence(-number, units) if number < 0
+
+      time = (
         case units.to_s.downcase.to_sym
         when :years
           set(:year => (year - number))
         when :months
-          #years = ((month - number) / 12).to_i
-          y = ((month - number) / 12).to_i
-          m = ((month - number - 1) % 12) + 1
-          set(:year => (year - y), :month => m)
+          new_month = ((month - number - 1) % 12) + 1
+          y = (number / 12) + (new_month > month ? 1 : 0)
+          set(:year => (year - y), :month => new_month)
         when :weeks
           self - (number * 604800)
         when :days
@@ -43,14 +44,16 @@ class Time
     # a number of time-units hence.
 
     def hence(number, units=:seconds)
-      time =(
+      return ago(-number, units) if number < 0
+
+      time = (
         case units.to_s.downcase.to_sym
         when :years
           set( :year=>(year + number) )
         when :months
-          y = ((month + number) / 12).to_i
-          m = ((month + number - 1) % 12) + 1
-          set(:year => (year + y), :month => m)
+          new_month = ((month + number - 1) % 12) + 1
+          y = (number / 12) + (new_month < month ? 1 : 0)
+          set(:year => (year + y), :month => new_month)
         when :weeks
           self + (number * 604800)
         when :days
@@ -76,7 +79,7 @@ class Time
     # TODO: Can't seem to get this to pass ActiveSupport tests.
     # Even though it is essentially identical to the
     # ActiveSupport code (see Time#since in time/calculations.rb).
-    # It handels all but 4 tests.
+    # It handles all but 4 tests.
     def dst_adjustment(time)
       self_dst = self.dst? ? 1 : 0
       time_dst = time.dst? ? 1 : 0
