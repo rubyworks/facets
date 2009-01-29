de5fd8002d5c2452b5dd46051d02d27044506e9b
trans <transfire@gmail.com>
2009-01-29 09:26:22 -0500

    fixed coruption of reciever by Hash#collate (thanks to Tilo Sloboda)

--- lib/core/facets/hash/collate.rb
+++ lib/core/facets/hash/collate.rb
@@ -6,40 +6,71 @@ class Hash
   #   { :a=>1, :b=>2 }.collate :a=>3, :b=>4, :c=>5
   #   #=> { :a=>[1,3], :b=>[2,4], :c=>[5] }
   #
+  # CREDIT: Tilo Sloboda
   # CREDIT: Gavin Kistner (Phrogz)
 
   def collate(other_hash)
-    dup.collate!(other_hash)
+    h = Hash.new
+    # Prepare, ensuring every existing key is already an Array
+    each do |key, value|
+      if value.is_a?(Array)
+        h[key] = value
+      else
+        h[key] = [value]
+      end
+    end
+    # Collate with values from other_hash
+    other_hash.each do |key, value|
+      if h[key]
+        if value.is_a?(Array)
+          h[key].concat(value)
+        else
+          h[key] << value
+        end
+      elsif value.is_a?(Array)
+        h[key] = value
+      else
+        h[key] = [value]
+      end
+    end
+    #each{ |key, value| value.uniq! } if options[ :uniq ]
+    h
   end
 
   # The same as #collate, but modifies the receiver in place.
 
   def collate!(other_hash)
-    # Prepare, ensuring every existing key is already an Array
-    each do |key, value|
-      if value.is_a?(Array)
-        self[key] = value
-      else
-        self[key] = [value]
-      end
-    end
-    # Collate with values from other_hash
-    other_hash.each do |key, value|
-      if self[key]
-        if value.is_a?(Array)
-          self[key].concat( value )
-        else
-          self[key] << value
-        end
-      elsif value.is_a?(Array)
-        self[key] = value
-      else
-        self[key] = [value]
-      end
-    end
-    #each{ |key, value| value.uniq! } if options[ :uniq ]
-    self
+    result = self.collate(other_hash)
+    self.replace(result)
   end
+
+  # Old version.
+  #def collate!(other_hash)
+  #  # Prepare, ensuring every existing key is already an Array
+  #  each do |key, value|
+  #    if value.is_a?(Array)
+  #      self[key] = value
+  #    else
+  #      self[key] = [value]
+  #    end
+  #  end
+  #  # Collate with values from other_hash
+  #  other_hash.each do |key, value|
+  #    if self[key]
+  #      if value.is_a?(Array)
+  #        self[key].concat( value )
+  #      else
+  #        self[key] << value
+  #      end
+  #    elsif value.is_a?(Array)
+  #      self[key] = value
+  #    else
+  #      self[key] = [value]
+  #    end
+  #  end
+  #  #each{ |key, value| value.uniq! } if options[ :uniq ]
+  #  self
+  #end
 
 end
 
