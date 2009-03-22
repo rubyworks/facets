078b7c80c76646af8399a72959d463f53be809cc
trans <transfire@gmail.com>
2009-03-22 06:02:41 -0400

    created separate test for Hash#dearray_values

--- test/core/test_to_hash.rb
+++ test/core/test_to_hash.rb
@@ -146,18 +146,6 @@ class TestHashConversion < Test::Unit::TestCase
     assert_equal( a, a.to_h )
   end
 
-  def test_dearray_values
-    h = { :a=>[1], :b=>[1,2], :c=>3, :d=>[] }
-    x = { :a=>1, :b=>1, :c=>3, :d=>nil }
-    assert_equal(x, h.dearray_values)    
-  end
-  
-  def test_dearray_singular_values
-    h = { :a=>[1], :b=>[1,2], :c=>3, :d=>[] }
-    x = { :a=>1, :b=>[1,2], :c=>3, :d=>nil }
-    assert_equal(x, h.dearray_singular_values )
-  end
-
 end
 
 
