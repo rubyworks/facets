4297b59346c1c4eddc1b991c0d15c15057a24487
trans <transfire@gmail.com>
2009-06-08 15:55:07 -0400

    added string/uppercase and lowercase

--- lib/core/facets/string/camelcase.rb
+++ lib/core/facets/string/camelcase.rb
@@ -2,23 +2,47 @@ class String
 
   # Converts a string to camelcase.
   #
-  # By default camelcase convert to UpperCamelCase, If an argument
-  # is set to +false+, then camelcase will produce lowerCamelCase.
+  # By default camelcase leaves the first charcter of the string as given.
+  # If +first_letter+ is set to +:lower+ or +false+, then +#camelcase+ will
+  # produce lowerCamelCase. If it is set to +:upper+ or +true+ it will
+  # produce UpperCamelCase.
   #
-  # +camelcase+ also converts '/' to '::' which is useful for converting
+  # +#camelcase+ also converts '/' to '::' which is useful for converting
   # paths to namespaces.
   #
   # Examples
-  #   "camel_case".camelcase                    #=> "CamelCase"
-  #   "camel/case".camelcase                    #=> "Camel::Case"
-  #   "camel_case".camelcase(false)             #=> "camelCase"
+  #   "camel_case".camelcase             #=> "CamelCase"
+  #   "camel/case".camelcase             #=> "Camel::Case"
+  #   "camel_case".camelcase(false)      #=> "camelCase"
   #
-  def camelcase(upcase_first_letter=true)
-    up = upcase_first_letter
+  def camelcase(first_letter=nil)
+    case first_letter
+    when :upper, true
+      upper_camelcase
+    when :lower, false
+      lower_camelcase
+    else
+      str = dup
+      str.gsub!(/\/(.?)/){ "::#{$1.upcase}" }  # NOT SO SURE ABOUT THIS
+      str.gsub!(/(?:_+|-+)([a-z])/){ $1.upcase }
+      #str.gsub!(/(\A|\s)([a-z])/){ $1 + $2.upcase }
+      str
+    end
+  end
+
+  def upper_camelcase
     str = dup
     str.gsub!(/\/(.?)/){ "::#{$1.upcase}" }  # NOT SO SURE ABOUT THIS
     str.gsub!(/(?:_+|-+)([a-z])/){ $1.upcase }
-    str.gsub!(/(\A|\s)([a-z])/){ $1 + $2.upcase } if up
+    str.gsub!(/(\A|\s)([a-z])/){ $1 + $2.upcase }
+    str
+  end
+
+  def lower_camelcase
+    str = dup
+    str.gsub!(/\/(.?)/){ "::#{$1.upcase}" }  # NOT SO SURE ABOUT THIS
+    str.gsub!(/(?:_+|-+)([a-z])/){ $1.upcase }
+    str.gsub!(/(\A|\s)([a-z])/){ $1 + $2.downcase }
     str
   end
 
