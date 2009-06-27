b61198728dc1face8553592fb26c88118a616def
trans <transfire@gmail.com>
2009-06-27 13:42:05 -0400

    moved ByteOrder into Platform

--- lib/more/facets/platform.rb
+++ lib/more/facets/platform.rb
@@ -188,5 +188,52 @@ class Platform
 
   CURRENT = 'current'
 
+  # Determine byte order of underlying machine.
+  def byte_order
+    ByteOrder.byte_order
+  end
+
+  def little_endian?
+    ByteOrder.little_endian?
+  end
+
+  def big_endian?
+    ByteOrder.big_endian?
+  end
+
+  # Copyright (c) 2003 Michael Neumann
+  module ByteOrder
+    Native    = :Native
+    BigEndian = Big = Network = :BigEndian
+    LittleEndian = Little = :LittleEndian
+
+    # examines the byte order of the underlying machine
+    def byte_order
+      if [0x12345678].pack("L") == "\x12\x34\x56\x78"
+        BigEndian
+      else
+        LittleEndian
+      end
+    end
+
+    alias_method :byteorder, :byte_order
+
+    def little_endian?
+      byte_order == LittleEndian
+    end
+
+    def big_endian?
+      byte_order == BigEndian
+    end
+
+    alias_method :little?, :little_endian?
+    alias_method :big?, :big_endian?
+    alias_method :network?, :big_endian?
+
+    module_function :byte_order, :byteorder
+    module_function :little_endian?, :little?
+    module_function :big_endian?, :big?, :network?
+  end
+
 end
 
