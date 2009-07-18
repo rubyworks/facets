b8f41ef510e58dc8e1845f29dcfbfd47fc192126
trans <transfire@gmail.com>
2009-07-18 12:47:55 -0400

    moved attr_validator to own files

--- lib/more/facets/attr.rb
+++ lib/more/facets/attr.rb
@@ -1,35 +1,5 @@
 class Module
 
-  # Like attr_writer, but the writer method validates the
-  # setting against the given block.
-  #
-  # CREDIT: ?
-
-  def attr_validator(*symbols, &validator)
-    made = []
-    symbols.each do |symbol|
-      define_method "#{symbol}=" do |val|
-        unless validator.call(val)
-          raise ArgumentError, "Invalid value provided for #{symbol}"
-        end
-        instance_variable_set("@#{symbol}", val)
-      end
-      made << "#{symbol}=".to_sym
-    end
-    made
-  end
-
-  # Create aliases for validators.
-
-  def alias_validator(*args)
-    orig = args.last
-    args = args - [orig]
-    args.each do |name|
-      #alias_method(name, orig)
-      alias_method("#{name}=", "#{orig}=")
-    end
-  end
-
   # Create a toggle attribute. This creates two methods for
   # each given name. One is a form of tester and the other
   # is used to toggle the value.
