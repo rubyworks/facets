6d960916fc99609c1766778708dd20064d70ecc3
trans <transfire@gmail.com>
2009-08-11 14:39:25 -0400

    fixed Proc#curry to work with define_method [bug]

--- test/core/proc/test_curry.rb
+++ test/core/proc/test_curry.rb
@@ -20,10 +20,14 @@ class TestProcCurry < Test::Unit::TestCase
     c = f.curry(1)
     assert_equal( 9, c[2][3] )
   end
-  
+
   def test_inclass_scope
+    # first test the lambda
+    org = lambda{ |y, x| x + " " + y }
+    foo = org.curry['yeah']
+    assert_equal('boo yeah', foo['boo'])
+    # now test it as a method definition
     baz = Class.new
-    foo = lambda {|y, x| puts x + " " + y}.curry['yeah']
     baz.__send__(:define_method, 'foo', foo)
     assert_equal('boo yeah',baz.new.foo('boo'))
   end
