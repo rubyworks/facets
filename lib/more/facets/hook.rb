18e91b9e5fadf77901f093bc488a495b7900a7ca
trans <transfire@gmail.com>
2009-08-12 07:14:05 -0400

    use instance_exec for signals

--- lib/more/facets/hook.rb
+++ lib/more/facets/hook.rb
@@ -1,3 +1,4 @@
+require 'facets/kernel/instance_exec'
 
 # TODO: hooks should be an inheritor
 #
@@ -24,7 +25,7 @@ module Hook
       def #{name}(*args)
         self.class.hooks[:#{name}].each do |blk|
           if Proc === blk
-            blk.call(:#{name}, *args)
+            instance_exec(:#{name}, *args, &blk)
           else
             __send__(blk, :#{name}, *args)
           end
@@ -34,32 +35,4 @@ module Hook
   end
 
 end
-
-
-=begin demo
-
-  class X
-    include Hook
-
-    hook :signal
-
-    def boom(event, message)
-      puts "Kaboom!"
-    end
-
-    signal do |event, message|
-      puts "1", event, message
-    end
-
-    signal do |event, message|
-      puts "2", event, message
-    end
-
-    signal :boom
-
-  end
-
-  X.new.signal("hi")
-
-=end
 
