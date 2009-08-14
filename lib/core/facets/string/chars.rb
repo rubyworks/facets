bfd8fd927f712e75b9a2b51a6d283bed7dfb2838
trans <transfire@gmail.com>
2009-08-14 07:27:40 -0400

    workaround String#chars under 1.9

--- lib/core/facets/string/chars.rb
+++ lib/core/facets/string/chars.rb
@@ -1,6 +1,10 @@
 class String
 
-  unless method_defined?(:chars) or defined?(::ActiveSupport) # 1.9 & ActiveSupport
+  # Some other library is defining this method but it is
+  # returning an Enumerator, not an array. Therefore we're
+  # overriding it regardless of it's inclusion in 1.9.
+  #
+  unless defined?(::ActiveSupport) # 1.9 & ActiveSupport
 
     # Returns an array of characters.
     #
