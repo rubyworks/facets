9e0ba9cfff88a938475aa3ed27d1d178637a2f5c
trans <transfire@gmail.com>
2009-09-25 16:11:09 -0400

    fixed kernel/d.rb (it's actually useful now)

--- lib/core/facets/kernel/d.rb
+++ lib/core/facets/kernel/d.rb
@@ -6,17 +6,18 @@ module Kernel
   #
   # produces
   #
-  #   /home/dave/projects/foo.rb, 38
-  #   "hi"
+  #   "hi" (/home/dave/projects/foo.rb, 38)
   #
-  # TODO: This is borderline "prime". Keep here?
-  # Another copy of it exits in dtools.rb
-
   def d(*x)
-    puts "#{__FILE__}, #{__LINE__}"
-    x.each{ |e| puts e.inspect } #p(*x)
-    x.size > 1 ? x : x.last #x.last
+    puts "#{x.inspect} #{caller[0]}"
+    return *x
   end
+
+  #def d(*x)
+  #  puts "#{__FILE__}, #{__LINE__}"
+  #  x.each{ |e| puts e.inspect } #p(*x)
+  #  x.size > 1 ? x : x.last #x.last
+  #end
 
 end
 
