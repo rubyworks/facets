cd8414b4662c6c51295c875839410d8d37fdaaec
trans <transfire@gmail.com>
2009-10-18 00:12:02 -0400

    renamed snd reworked Shellwords extensions

--- lib/more/facets/shellwords.rb
+++ lib/more/facets/shellwords.rb
@@ -1,24 +1,3 @@
-# = Shellwords Extended
-#
-# Adds extensions to Shellwords, namely #escape.
-#
-# == Authors
-#
-# * Thomas Sawuer
-#
-# == Copying
-#
-# Copyright (c) 2007 Thomas Sawyer
-#
-# Ruby License
-#
-# This module is free software. You may use, modify, and/or redistribute this
-# software under the same terms as Ruby.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.
-
 require 'shellwords'
 
 module Shellwords
@@ -34,56 +13,50 @@ module Shellwords
 
 end
 
-
 class Array
 
   # Convert an array into command line parameters.
   # The array is accepted in the format of Ruby
   # method arguments --ie. [arg1, arg2, ..., hash]
-
-  def to_shellwords
-    flags = (Hash===last ? pop : {})
-    flags = flags.to_shellwords
-    flags + ' ' + self #join(" ")
+  #
+  def shellwords
+    opts, args = *flatten.partition{ |e| Hash === e }
+    opts = opts.inject({}){ |m,h| m.update(h); m }
+    opts.shellwords + args
   end
 
-  def to_shell
-    to_shellwords.join(' ')
+  def shelljoin
+    Shellwords.shelljoin(shellwords)
   end
 
-  # Original name.
-  alias_method :to_console, :to_shell
 end
-
 
 class Hash
 
-  # Convert an array into command line parameters.
-  # The array is accepted in the format of Ruby
-  # method arguments --ie. [arg1, arg2, ..., hash]
-
-  def to_shellwords
-    flags = collect do |f,v|
+  #
+  def shellwords
+    argv = []
+    each do |f,v|
       m = f.to_s.size == 1 ? '-' : '--'
       case v
-      when Array
-        v.collect{ |e| "#{m}#{f}='#{e}'" }.join(' ')
-      when true
-        "#{m}#{f}"
       when false, nil
-        ''
+      when Array
+        v.each do |e|
+          argv << %[#{m}#{f}="#{e}"]
+        end
+      when true
+        argv << %[#{m}#{f}]
       else
-        "#{m}#{f}='#{v}'"
+        argv << %[#{m}#{f}="#{v}"]
       end
     end
-    flags #.join(" ")
+    argv
   end
 
-  def to_shell
-    to_shellwords.join(' ')
+  #
+  def shelljoin
+    shellwords.shelljoin
   end
 
-  # Original name.
-  alias_method :to_console, :to_shell
 end
 
---
+++ test/more/test_shellwords.rb
@@ -0,0 +1,31 @@
+require 'facets/shellwords'
+require 'test/unit'
+
+class TestShellwords < Test::Unit::TestCase
+
+  def test_array_shellwords_1
+    e = ["a", "b"]
+    a = ["a", "b"].shellwords
+    assert_equal(e, a)
+  end
+
+  def test_array_shellwords_2
+    e = ["-d", "a", "b"]
+    a = ["a", "b", {:d=>true}].shellwords
+    assert_equal(e, a)
+  end
+
+  def test_hash_shellwords_1
+    e = ['--debug']
+    a = {:debug=>true}.shellwords
+    assert_equal(e, a)
+  end
+
+  def test_hash_shellwords_2
+    e = ['--name="jim"', '--debug']
+    a = {:debug=>true, :name=>"jim"}.shellwords
+    assert_equal(e, a)
+  end
+
+end
+
