e41c3a7155972b75c6356f67587efcd0c743f7d8
trans <transfire@gmail.com>
2009-11-02 23:56:02 -0500

    looks like id changed after 1.8.6

--- lib/core/facets/kernel/object_hexid.rb
+++ lib/core/facets/kernel/object_hexid.rb
@@ -1,12 +1,27 @@
 module Kernel
 
-  # Returns the object id as a string in hexideciaml,
-  # which is how Ruby reports them with inspect.
-  #
-  #   "ABC".object_hexid  #=> "0x402d359c"
+  if RUBY_VERSION < "1.8.7"
 
-  def object_hexid
-    return "0x" << ('%.x' % (2*self.__id__))  #[1..-1]
+    # Returns the object id as a string in hexideciaml,
+    # which is how Ruby reports them with inspect.
+    #
+    #   "ABC".object_hexid  #=> "0x402d359c"
+
+    def object_hexid
+      return "0x" << (('%.x' % (2*self.__id__))[1..-1])
+    end
+
+  else
+
+    # Returns the object id as a string in hexideciaml,
+    # which is how Ruby reports them with inspect.
+    #
+    #   "ABC".object_hexid  #=> "0x402d359c"
+
+    def object_hexid
+      return "0x" << ('%.x' % (2*self.__id__))  #[1..-1]
+    end
+
   end
 
 end
