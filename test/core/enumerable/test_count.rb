c15db7228ae402012f080c45877c6b7da4c86474
trans <transfire@gmail.com>
2009-11-03 00:08:45 -0500

    is it some bad encoding?

--- test/core/enumerable/test_count.rb
+++ test/core/enumerable/test_count.rb
@@ -4,25 +4,25 @@ require 'test/unit'
 class TC_Enumerable_Count < Test::Unit::TestCase
 
   def test_count_strings
-    e = [ 'a', '1', 'a' ]
+    e = ['a','1','a']
     r = e.count('1')
     assert_equal(1, r)
   end
 
   def test_count_strings_again
-    e = [ 'a', '1', 'a' ]
+    e = ['a','1','a']
     r = e.count('a')
-    assert_equal(2, r)
+    assert_equal(2,r)
   end
 
   def test_count_array_elements
-    e = [['a',2], ['a',2], ['a',2], ['b',1]]
+    e = [['a',2],['a',2],['a',2],['b',1]]
     r = e.count(['a',2])
-    assert_equal(3, r)
+    assert_equal(3,r)
   end
 
   def test_count_hash
-    e = { 'a' => 2, 'a' => 2, 'b' => 1 }
+    e = { 'a'=>2, 'a'=>2, 'b'=>1 }
     r = e.count(['a',2])
     assert_equal(2, r)
   end
