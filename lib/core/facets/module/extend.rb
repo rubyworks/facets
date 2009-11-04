57f8eb36d929376d5f2de8fcd6318194861f19bf
trans <transfire@gmail.com>
2009-11-04 15:48:58 -0500

    extended #extend

--- lib/core/facets/module/extend.rb
+++ lib/core/facets/module/extend.rb
@@ -1,10 +1,10 @@
-class Module
-
-  alias_method :_extend, :extend
-
-  def extend(*mod, &blk)
-    _extend *mod unless mod.empty?
-    _extend Module.new(&blk) if blk
-  end
-
-end
+#class Module
+#
+#  alias_method :_extend, :extend
+#
+#  def extend(*mod, &blk)
+#    _extend *mod unless mod.empty?
+#    _extend Module.new(&blk) if blk
+#  end
+#
+#end
