8a02624dacdf2925ea000bdc1dc02ddcdaf0273e
trans <transfire@gmail.com>
2009-11-04 20:28:14 -0500

    string/lines.rb, use each_line [bug]

--- lib/core/facets/string/lines.rb
+++ lib/core/facets/string/lines.rb
@@ -4,16 +4,13 @@ class String
 
     # Returns an array of characters.
     #
-    #   "abc\n123".lines  #=> ["abc","123"]
-    #
-    # Note, this is not 100% compatible with 1.8.7+
-    # which returns an enumerator instead of an array.
+    #   "abc\n123".lines  #=> ["abc\n","123"]
     #
     def lines(&blk)
       if block_given?
-        self.split(/\n/).each(&blk)
+        each_line(&blk) #scan(/$.*?\n/).each(&blk)
       else
-        self.split(/\n/)
+        Enumerator.new(self, :lines) #.split(/\n/)
       end
     end
 
