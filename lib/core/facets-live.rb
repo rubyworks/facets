735787a7d27e2478a5ed1f0781532ef1071af2b0
trans <transfire@gmail.com>
2009-11-11 12:04:19 -0500

    fixed facets-live.rb [bug]

--- lib/core/facets-live.rb
+++ lib/core/facets-live.rb
@@ -1,13 +1,15 @@
+require 'facets/opesc'
+
 class Object
 
+  # This will automatically load (most) core methods
+  # if they are not present when called.
+
   def method_missing(methodname, *a, &b)
-    # TODO Use #op_esc convert symbolic method names.
+    methodname = OpEsc.escape(methodname)
     begin
       require "facets/#{class}/#{methodname}"
-      # TODO Would it be better to use #methods.include? ?
-      if respond_to?(methodname)
-        send(methodname, *a, &b)
-      end
+      __send__(methodname, *a, &b)
     rescue LoadError
       super
     end  
