30c30a1eca2f069e321660db2916567477594c3b
trans <transfire@gmail.com>
2009-11-11 13:49:09 -0500

    test UnboundMethod#name even if in Ruby 1.9

--- test/core/unboundmethod/test_name.rb
+++ test/core/unboundmethod/test_name.rb
@@ -9,7 +9,7 @@ class Test_UnboundMethod_Name < Test::Unit::TestCase
   def test_name
     meth = X.instance_method(:foo)
     assert_equal("foo", meth.name)
-  end if RUBY_VERSION < "1.9" # UnboundMethod#name is defined already in 1.9.
+  end  #if RUBY_VERSION < "1.9" # UnboundMethod#name is defined already in 1.9.
 
 end
 
