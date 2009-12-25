7ba82fd8b7ae76c5f7fbeaf1d26af7fb5749fbf4
trans <transfire@gmail.com>
2009-12-25 08:48:17 -0500

    added #glob_relative to Pathname

--- lib/more/facets/pathname.rb
+++ lib/more/facets/pathname.rb
@@ -86,6 +86,68 @@ class Pathname
     self.class.new(File.rootname(to_s))
   end
 
+  #
+  def split_root
+    head, tail = *::File.split_root(to_s)
+    [self.class.new(head), self.class.new(tail)]
+  end
+
+  # Glob pathnames.
+  def glob(match, *opts)
+    flags = glob_flags(opts)
+    Dir.glob(::File.join(self.to_s, match), flags).collect{ |m| self.class.new(m) }
+  end
+
+  # Return the first glob match.
+  #
+  # DEPRECATE: While slightly faster then glob().first, not really worth it
+  # unless this can be rewritten to shortcut on first match (using fnmatch?).
+  # In wich case, is there a better name for this method?
+  def glob_first(match, *opts)
+    flags = glob_flags(opts)
+    file = ::Dir.glob(::File.join(self.to_s, match), flags).first
+    file ? self.class.new(file) : nil
+  end
+
+  # Return globbed matches with pathnames relative to the current pathname.
+  def glob_relative(match, *opts)
+    flags = glob_flags(opts)
+    files = Dir.glob(::File.join(self.to_s, match), flags)
+    files = files.map{ |f| f.sub(self.to_s.chomp('/') + '/', '') }
+    files.collect{ |m| self.class.new(m) }
+  end
+
+  private
+
+  def glob_flags(opts)
+    flags = 0
+    opts.each do |opt|
+      case opt when Symbol, String
+        flags += ::File.const_get("FNM_#{opt}".upcase)
+      else
+        flags += opt
+      end
+    end
+    flags
+  end
+
+  public
+
+  #
+  def empty?
+    Dir.glob(::File.join(self.to_s, '*')).empty?
+  end
+
+  #
+  def uptodate?(*sources)
+    ::FileUtils.uptodate?(to_s, sources.flatten)
+  end
+
+  #
+  def outofdate?(*sources)
+    ::FileUtils.outofdate?(to_s, sources.flatten)
+  end
+
 #   # Already included in 1.8.4+ version of Ruby (except for inclusion flag)
 #   if not instance_methods.include?(:ascend)
 #
@@ -117,54 +179,6 @@ class Pathname
 #     end
 #
 #   end
-
-  #
-  def split_root
-    head, tail = *::File.split_root(to_s)
-    [self.class.new(head), self.class.new(tail)]
-  end
-
-  #
-  def glob(match, *opts)
-    flags = 0
-    opts.each do |opt|
-      case opt when Symbol, String
-        flags += ::File.const_get("FNM_#{opt}".upcase)
-      else
-        flags += opt
-      end
-    end
-    Dir.glob(::File.join(self.to_s, match), flags).collect{ |m| self.class.new(m) }
-  end
-
-  #
-  def glob_first(match, *opts)
-    flags = 0
-    opts.each do |opt|
-      case opt when Symbol, String
-        flags += ::File.const_get("FNM_#{opt}".upcase)
-      else
-        flags += opt
-      end
-    end
-    file = ::Dir.glob(::File.join(self.to_s, match), flags).first
-    file ? self.class.new(file) : nil
-  end
-
-  #
-  def empty?
-    Dir.glob(::File.join(self.to_s, '*')).empty?
-  end
-
-  #
-  def uptodate?(*sources)
-    ::FileUtils.uptodate?(to_s, sources.flatten)
-  end
-
-  #
-  def outofdate?(*sources)
-    ::FileUtils.outofdate?(to_s, sources.flatten)
-  end
 
 end
 
