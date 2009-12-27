f2832c4de21b06c233165e762cc5dedb98dac54a
trans <transfire@gmail.com>
2009-12-27 11:42:44 -0500

    [minor] Removed #** alias for Array#product. If Array#* were available, we could use it. As it is this is too much of a operator misnomer, as #** is used by Numeric to calculate exponential power. Such an operator makes more sense for a Matrix class and scientific application in anycase, so is overkill for Array.

--- lib/core/facets/array/product.rb
+++ lib/core/facets/array/product.rb
@@ -27,14 +27,5 @@ class Array
 
   end
 
-  # Operator alias for cross-product.
-  #
-  #   a = [1,2] ** [4,5]
-  #   a  #=> [[1, 4],[1, 5],[2, 4],[2, 5]]
-  #
-  # CREDIT: Trans
-
-  alias_method :**, :product
-
 end
 
