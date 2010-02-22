2a9230e75d4be1bb2dd4b2763b19e13534f818da
trans <transfire@gmail.com>
2010-02-22 13:20:18 -0500

    bug: attempt to fix UnboundMethod#name test fail for Ruby 1.9

--- lib/core/facets/unboundmethod/name.rb
+++ lib/core/facets/unboundmethod/name.rb
@@ -1,24 +1,28 @@
 class UnboundMethod
 
-  # Return the name of the method.
-  #
-  # Be aware that in ruby 1.9 UnboundMethod#name is defined already,
-  # but it returns a Symbol not a String.
-  #
-  #   class X
-  #     def foo; end
-  #   end
-  #
-  #   meth = X.instance_method(:foo)
-  #
-  #   meth.name  #=> "foo"
-  #
-  # CREDIT: Trans
+  unless method_defined?(:name) # 1.8.7+
 
-  def name
-    i = to_s.rindex('#')
-    to_s.slice(i+1...-1)
-  end unless method_defined?(:name) # 1.8.7+
+    # Return the name of the method.
+    #
+    # Be aware that in ruby 1.9 UnboundMethod#name is defined already,
+    # but it returns a Symbol not a String.
+    #
+    #   class X
+    #     def foo; end
+    #   end
+    #
+    #   meth = X.instance_method(:foo)
+    #
+    #   meth.name  #=> :foo
+    #
+    # CREDIT: Trans
+
+    def name
+      i = to_s.rindex('#')
+      to_s.slice(i+1...-1).to_sym
+    end
+
+  end
 
 end
 
--- test/core/unboundmethod/test_name.rb
+++ test/core/unboundmethod/test_name.rb
@@ -3,13 +3,13 @@ require 'facets/unboundmethod/name'
 class Test_UnboundMethod_Name < Test::Unit::TestCase
 
   class X
-    def foo; end
+    def foo; "bar"; end
   end
 
   def test_name
     meth = X.instance_method(:foo)
-    assert_equal("foo", meth.name)
-  end  #if RUBY_VERSION < "1.9" # UnboundMethod#name is defined already in 1.9.
+    assert_equal(:foo, meth.name)
+  end
 
 end
 
