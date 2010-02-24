f543661b901ce85b53a859c4ce979ee4ece864d2
trans <transfire@gmail.com>
2010-02-24 11:58:18 -0500

    doc: improve doc on Numeric#spacing

--- lib/core/facets/numeric/spacing.rb
+++ lib/core/facets/numeric/spacing.rb
@@ -1,18 +1,16 @@
-# Calculate the number of digits in an integer.
-#
-#      1.spacing   #=> 1
-#     10.spacing   #=> 2
-#    100.spacing   #=> 3
-#   -100.spacing   #=> 4
-#    1.2.spacing   #=> 3
-#
-# CREDIT: Victor H. Goff III
-
 class Numeric
 
-  # Returns the size of the string representation
-  # of the number.
+  # Returns the size of the string representation of
+  # a numerical value.
   #
+  #      1.spacing   #=> 1
+  #     10.spacing   #=> 2
+  #    100.spacing   #=> 3
+  #   -100.spacing   #=> 4
+  #    1.2.spacing   #=> 3
+  #
+  # CREDIT: Victor H. Goff III
+
   def spacing
     to_s.length
   end
