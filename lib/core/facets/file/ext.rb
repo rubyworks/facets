6ba94a2be325a642c9983e9fe5814de2413bf58d
trans <transfire@gmail.com>
2010-04-02 12:12:22 -0400

    minor: a bit more improvement to File#ext

--- lib/core/facets/file/ext.rb
+++ lib/core/facets/file/ext.rb
@@ -2,36 +2,33 @@ class File
 
   # Takes a file name string and returns or changes its extension.
   #
-  #   ext()
+  # Without a new extension argument, returns the extension of the
+  # file name. In this respect #ext is like #extname, but unlike
+  # #extname it does not include the dot prefix.
   #
-  # returns extension of this file name.
-  #
-  #   ext(new_ext)
-  #
-  # changes exension of this file name to _new_ext_ and returns changed
-  # file name.
+  # With a new extension argument, changes the exension of the file
+  # name to the new extension and returns it.
   #
   # Examples:
   #
   #   File.ext('file.rb')          => 'rb'
   #   File.ext('file.rb', 'txt')   => 'file.txt'
+  #   File.ext('file.rb', '.txt')  => 'file.txt'
   #   File.ext('file.rb', '')      => 'file'
   #
-  # Can be used with String#file for more object-oriented approach.
+  # This method can be used with String#file for more object-oriented notation.
   #
   #   'file.rb'.file.ext('txt')   => 'file.txt'
   #
-  # Unlike #extname this method does not include the extension dot.
+  # CREDIT: Lavir the Whiolet
 
-  def self.ext(string, new_ext=nil)
+  def self.ext(filename, new_ext=nil)
+    old_ext = extname(filename)
     if new_ext == nil
-      extname(string).sub(/^\./, '')
+      old_ext.sub(/^\./, '')
     else
-      old_ext = File.extname(string)
-      new_ext = '.' + new_ext unless (new_ext[0,1] == '.' || new_ext.empty?)
-      string.chomp(old_ext) + new_ext
-      #if old_ext == '' then return string + new_ext
-      #else return string[0...-old_ext.length] + new_ext; end
+      new_ext = '.' + new_ext unless (new_ext.empty? || new_ext[0,1] == '.')
+      filename.chomp(old_ext) + new_ext
     end
   end
 
