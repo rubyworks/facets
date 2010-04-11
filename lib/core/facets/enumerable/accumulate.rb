6350986a95cdb8b090b5804e861d95e9cae3811e
Daniel <ddavis@israstage.com>
2010-04-11 16:34:38 +0800

    chain accumulations

--- lib/core/facets/enumerable/accumulate.rb
+++ lib/core/facets/enumerable/accumulate.rb
@@ -10,12 +10,18 @@ module Enumerable
   #   groups.accumulate.users
   #
   # will return a list of users from all groups.
+  # 
+  # you may pass an argument to perform chains, e.g.
+  #  groups.accumulate(2).users.name #=> names of users from all groups
+  #  OR groups.accumulate(2).users.friends #=> all the friends of all users in groups
   #
   # CREDIT: George Moshchovitis
 
-  def accumulate
+  def accumulate(iterations = 1)
+    return self if iterations == 0
     @_accumulate ||= Functor.new do |op, *args|
-      inject([]) { |a, x| a << x.send(op, *args) }.flatten
+      result = inject([]) { |a, x| a << x.send(op, *args) }.flatten
+      result.accumulate(iterations - 1)
     end
   end
 
