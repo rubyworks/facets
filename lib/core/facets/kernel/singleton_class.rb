ecd64aeecc9cd1551478df1139eea05a1041f3d6
trans <transfire@gmail.com>
2010-04-16 03:15:12 -0400

    minor: for compatability with 1.9.2, #singleton_class no longer can take a block

--- lib/core/facets/kernel/singleton_class.rb
+++ lib/core/facets/kernel/singleton_class.rb
@@ -1,19 +1,20 @@
 module Kernel
 
-  # Easy access to an object's "special" class,
+  # Easy access to an object's "special" class.
   #
-  # One day these names must be reconciled!
+  # <b>This is part of Ruby as of 1.9.2+.</b>
+  #
+  # NOTE: This used to support a class eval block,
+  # but to comply with Ruby 1.9.2's definition
+  # it has been removed.
 
   def singleton_class(&block)
-    if block_given?
-      (class << self; self; end).class_eval(&block)
-    else
+    #if block_given?
+    #  (class << self; self; end).class_eval(&block)
+    #else
       (class << self; self; end)
-    end
-  end
-
-  # Deprecate?
-  alias_method :singleton, :singleton_class
+    #end
+  end unless method_defined? :singleton_class
 
 end
 
