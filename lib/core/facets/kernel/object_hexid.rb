a795a3edd62239c15d5ba8228178158faa8b0a67
trans <transfire@gmail.com>
2010-04-24 12:21:13 -0400

    minor: simplified #object_hexid

--- lib/core/facets/kernel/object_hexid.rb
+++ lib/core/facets/kernel/object_hexid.rb
@@ -1,27 +1,13 @@
 module Kernel
 
-  if RUBY_VERSION < "1.8.7"
-
-    # Returns the object id as a string in hexideciaml,
-    # which is how Ruby reports them with inspect.
-    #
-    #   "ABC".object_hexid  #=> "0x402d359c"
-
-    def object_hexid
-      return "0x" << (('%.x' % (2*self.__id__))[1..-1])
-    end
-
-  else
-
-    # Returns the object id as a string in hexideciaml,
-    # which is how Ruby reports them with inspect.
-    #
-    #   "ABC".object_hexid  #=> "0x402d359c"
-
-    def object_hexid
-      return "0x" << ('%.x' % (2*self.__id__))  #[1..-1]
-    end
-
+  # Returns the object id as a string in hexideciaml,
+  # which is how Ruby reports them with inspect.
+  #
+  #   "ABC".object_hexid  #=> "0x402d359c"
+  #
+  def object_hexid
+    "0x" << (__id__ << 1).to_s(16)
   end
 
 end
+
