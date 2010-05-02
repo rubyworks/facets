d8b5898ddd4c0ec18ff11b02e44399938fa075e1
trans <transfire@gmail.com>
2010-05-02 13:14:22 -0400

    minor: fix doc for array/uniq_by.rb

--- lib/core/facets/array/uniq_by.rb
+++ lib/core/facets/array/uniq_by.rb
@@ -1,17 +1,16 @@
-require 'facets/enumerable/uniq_by'
+require 'facets/array/uniq_by'
 
 class Array
 
   # Like #uniq, but determines uniqueness based on a given block.
   #
-  #   (-5..5).to_a.uniq_by {|i| i*i }
-  #
-  # produces
-  #
-  #   [-5, -4, -3, -2, -1, 0]
+  #   a = (-5..5).to_a
+  #   a.uniq_by!{ |i| i*i }
+  #   a #=> [-5, -4, -3, -2, -1, 0]
   #
   def uniq_by! #:yield:
-    h = {}; replace(inject([]){|a,x| h[yield(x)] ||= a << x})
+    h = {}
+    replace( inject([]){|a,x| h[yield(x)] ||= a << x} )
   end
 
 end
