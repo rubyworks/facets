e77049b4fdb624c406a71098fc23584d78f9db4c
trans <transfire@gmail.com>
2010-05-02 09:54:33 -0400

    move: duplicable.rb moves to kernel/dup.rb

--- lib/core/facets/kernel/dup.rb
+++ lib/core/facets/kernel/dup.rb
@@ -1,1 +1,34 @@
 require 'facets/kernel/try_dup'
+
+class Object
+  # Can you safely call #dup on this object?
+  # False for nil, false, true, symbols, and numbers; true otherwise.
+  def dup?   ; true ; end
+  def clone? ; true ; end
+end
+
+class NilClass
+  def dup?   ; false ; end
+  def clone? ; false ; end
+end
+
+class FalseClass
+  def dup?   ; false ; end
+  def clone? ; false ; end
+end
+
+class TrueClass
+  def dup?   ; false ; end
+  def clone? ; false ; end
+end
+
+class Symbol
+  def dup?   ; false ; end
+  def clone? ; false ; end
+end
+
+class Numeric
+  def dup?   ; false ; end
+  def clone? ; false ; end
+end
+
