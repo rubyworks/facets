a7678e57515c6b15125ecd7b8781588e6b805af6
trans <transfire@gmail.com>
2010-05-02 11:34:20 -0400

    feature: Use Comparable.[] instead of Comparable()

--- lib/core/facets/comparable/op_get.rb
+++ lib/core/facets/comparable/op_get.rb
@@ -1,5 +1,35 @@
-# It would nice if we could the following. Then the middle portion of the #Comparable
-# method would not be needed. But I fear it might break others code.
+module Comparable
+
+  # Automatically generate comparitive definitions based on
+  # attribute fields.
+  #
+  #   include Comparable[:a, :b]
+  #
+  # is equivalent to including a module containing:
+  #
+  #   def <=>(other)
+  #     cmp = self.a <=> other.a; return cmp unless cmp == 0
+  #     cmp = self.b <=> other.b; return cmp unless cmp == 0
+  #     0
+  #   end
+  #
+  def self.[](*accessors)
+    Module.new do
+      include Comparable
+      define_method(:comparability){ accessors }
+      define_method(:<=>) do |other|
+        comparability.each do |a|
+          cmp = (send(a) <=> other.send(a))
+          break cmp unless cmp == 0
+        end
+      end
+    end
+  end
+
+end
+
+# Would it be nice if we could define comparability for all objects?
+# Then Comparable[] method would not be needed. Just:
 #
 #   module Comparable
 #
@@ -10,33 +40,6 @@
 #    end
 #
 #  end
-
-class Module
-
-  # Automatically generate sorting definitions based on attribute fields.
-  #
-  #   include Comparable(:a, :b)
-  #
-  # is equivalent to including a module containing:
-  #
-  #   def <=>(other)
-  #     cmp = self.a <=> other.a; return cmp unless cmp == 0
-  #     cmp = self.b <=> other.b; return cmp unless cmp == 0
-  #     0
-  #   end
-  #
-  def Comparable(*accessors)
-    define_method(:comparability){ accessors }
-    code = %{
-      def <=>(other)
-        comparability.each do |a|
-          cmp = (send(a) <=> other.send(a)); return cmp unless cmp == 0
-        end
-      end
-    }
-    module_eval code
-    return Comparable
-  end
-
-end
+#
+# But I fear it might break other code.
 
