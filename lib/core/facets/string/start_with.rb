e9d57c5810ef1d1f75b98a36c889ad3cd1c22adc
trans <transfire@gmail.com>
2010-06-04 07:54:50 -0400

    now String#start_with? and #end_with? can take Regex argument

--- lib/core/facets/string/start_with.rb
+++ lib/core/facets/string/start_with.rb
@@ -1,34 +1,57 @@
+#--
+# = Developer's Notes
+#
+# Old definition of start_with? written by Lucas Carlson
+# and Blaine Cook was:
+#
+#   index(prefix) == 0
+# 
+# I like the simplicity of this definition, but I could not
+# find a way define #end_with? in similar terms and still
+# accept regular expressions for the suffix. So I had to use
+# Regexp match. On th upside, it can return MatchData
+# which might be more useful.
+#++
+
 class String
 
-  unless method_defined?(:start_with?) # 1.8.7+
+  #unless method_defined?(:start_with?) # 1.8.7+
 
     # Does a string start with the given prefix?
     #
     #   "hello".start_with?("he")    #=> true
     #   "hello".start_with?("to")    #=> false
     #
-    # CREDIT: Lucas Carlson, Blaine Cook
+    # Note: This definition is better than standard Ruby's
+    # becuase it handles regular expressions.
+    #
+    # CREDIT: Juris Galang
 
     def start_with?(prefix)
-      self.index(prefix) == 0
+      suffix = Regexp.escape(suffix.to_s) unless Regexp===suffix
+      /^#{suffix}/.match(self) ? true : false
     end
 
-  end
+  #end
 
-  unless method_defined?(:end_with?) # 1.8.7+
+  #unless method_defined?(:end_with?) # 1.8.7+
 
     # Does a string end with the given suffix?
     #
     #   "hello".end_with?("lo")    #=> true
     #   "hello".end_with?("to")    #=> false
     #
-    # CREDIT: Lucas Carlson, Blaine Cook
+    # Note: This definition is better than standard Ruby's
+    # becuase it handles regular expressions.
+    #
+    # CREDIT: Juris Galang
 
     def end_with?(suffix)
-      self.rindex(suffix) == size - suffix.size
+      suffix = Regexp.escape(suffix.to_s) unless Regexp===suffix
+      /#{suffix}$/.match(self) ? true : false
     end
 
-  end
+  #end
 
   alias_method :starts_with?, :start_with?
   alias_method :ends_with?  , :end_with?
