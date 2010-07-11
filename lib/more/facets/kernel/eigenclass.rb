82b73ddeb35509b82e26fd2d2454fdf346d5fe28
trans <transfire@gmail.com>
2010-07-11 08:12:18 -0400

    eigenclass is now in More (hence optional). sorry _why, but you gave it up.

--- lib/more/facets/kernel/eigenclass.rb
+++ lib/more/facets/kernel/eigenclass.rb
@@ -2,13 +2,11 @@ module Kernel
 
   # During this trying time when no one can get their
   # techie catchwords to stick to the refrigerator no
-  # matter how hard they slap it # with the enchanted
+  # matter how hard they slap it with the enchanted
   # magnetic spatula, it’s good to know that the
   # contrived phrases really do fly, graceful and
   # unclasped and bearing north toward chilled shrimp.
   # I know what my Hallowe’en pumpkin is going to say.
-  #
-  #                       -- why the lucky stiff
   #
   # CREDIT: WhyTheLuckyStiff
 
