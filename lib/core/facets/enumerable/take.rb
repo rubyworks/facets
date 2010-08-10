e3e9925790823badaece3dea9cc8b8b6612a1a0c
trans <transfire@gmail.com>
2010-08-09 21:15:22 -0400

    use method_defined condition for Enumerable#take

--- lib/core/facets/enumerable/take.rb
+++ lib/core/facets/enumerable/take.rb
@@ -1,20 +1,16 @@
 module Enumerable
 
-  if RUBY_VERSION < "1.9"
-
-    # Return the first n items from the collection
-    #
-    def take(n)
-      res = []
-      count = 0
-      each do |e|
-        break if count >= n
-        res << e
-        count += 1
-      end
-      res
+  # Return the first n items from the collection
+  #
+  def take(n)
+    res = []
+    count = 0
+    each do |e|
+      break if count >= n
+      res << e
+      count += 1
     end
-
-  end
+    res
+  end unless method_defined?(:take)
 
 end
