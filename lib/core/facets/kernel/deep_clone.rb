ed94a58d0ac4ffc6c43e019fe983bb57bde20b3b
trans <transfire@gmail.com>
2010-08-11 10:52:15 -0400

    add Kernel#deep_clone method

---
+++ lib/core/facets/kernel/deep_clone.rb
@@ -0,0 +1,1 @@
+require 'facets/kernel/deep_copy'
--- lib/core/facets/kernel/deep_copy.rb
+++ lib/core/facets/kernel/deep_copy.rb
@@ -10,5 +10,29 @@ module Kernel
     Marshal::load(Marshal::dump(self))
   end
 
+  #
+  def deep_clone(cache={})
+    return cache[self] if cache.key?(self) #@deep_cloning_obj if @deep_cloning
+    copy = clone
+    cache[self] = copy #@deep_cloning_obj = clone
+    copy.instance_variables.each do |var|
+      val = instance_variable_get(var)
+      begin
+        #@deep_cloning = true
+        val = val.deep_clone(cache)
+      rescue TypeError
+        next
+      ensure
+        #@deep_cloning = false
+      end
+      copy.instance_variable_set(var, val)
+      #@deep_cloning_obj.instance_variable_set(var, val)
+    end
+    #deep_cloning_obj = @deep_cloning_obj
+    #@deep_cloning_obj = nil
+    #deep_cloning_obj
+    copy
+  end
+
 end
 
