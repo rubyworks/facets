f2233356bb6a6b4c798e750be515b0eb98401cd7
trans <transfire@gmail.com>
2010-08-11 10:48:38 -0400

    rename some mappings in OpEsc

--- lib/more/facets/opesc.rb
+++ lib/more/facets/opesc.rb
@@ -1,25 +1,38 @@
 # = OpEsc
 #
+# NOTE: As of v3.0 of Facets these have been renamed:
+#
+#    op_plus_self   -> op_plus
+#    op_minus_self  -> op_minus
+#    op_plus        -> op_add
+#    op_minus       -> op_sub
+#    op_case_eq     -> op_case
+#
+# TODO: In the future we might also rename:
+#
+#    op_lshift      -> op_push
+#    op_rshift      -> op_pull
+#
 module OpEsc
 
   OPERATORS = %w{ +@ -@ + - ** * / % ~ <=> << >> < > === == =~ <= >= | & ^ []= [] }
   OPERATORS_REGEXP = Regexp.new( '(' << OPERATORS.collect{ |k| Regexp.escape(k) }.join('|') << ')' )
   OPERATORS_ESC_TABLE = {
-      "+@"   => "op_plus_self",
-      "-@"   => "op_minus_self",
-      "+"    => "op_plus",
-      "-"    => "op_minus",
+      "+@"   => "op_plus",
+      "-@"   => "op_minus",
+      "+"    => "op_add",
+      "-"    => "op_sub",
       "**"   => "op_pow",
       "*"    => "op_mul",
       "/"    => "op_div",
       "%"    => "op_mod",
       "~"    => "op_tilde",
       "<=>"  => "op_cmp",
-      "<<"   => "op_lshift",
-      ">>"   => "op_rshift",
+      "<<"   => "op_lshift",  #push?
+      ">>"   => "op_rshift",  #pull?
       "<"    => "op_lt",
       ">"    => "op_gt",
-      "==="  => "op_case_eq",
+      "==="  => "op_case",
       "=="   => "op_equal",
       "=~"   => "op_apply",
       "<="   => "op_lt_eq",
@@ -41,8 +54,13 @@ module OpEsc
     str.gsub(OPERATORS_REGEXP){ OPERATORS_ESC_TABLE[$1] }
   end
 
-  # NOTE: op_esc used to support the method require_esc.
   #
+  def self.method_to_filename(name)
+    fname = escape(name)
+    fname = fname[0...-1] if fname =~ /[\!\=\?]$/
+    fname
+  end
+
   #   # Require a file with puncuation marks escaped.
   #   #
   #   #   require_esc '[].rb'
@@ -51,10 +69,9 @@ module OpEsc
   #
   #   def require_esc( fpath )
   #     fdir, fname = File.split(fpath)
-  #     ename = op_esc( fname )
-  #     case ename[-1,1] ; when '!','=','?' then ename = ename[0...-1] ; end
-  #     epath = File.join( fdir, ename )
-  #     require( epath )
+  #     fname = method_to_filename(fname)
+  #     path = File.join(fdir, fname)
+  #     require(path)
   #   end
 
 end
