f8d727cc3d0e1896b4cecc757ecfe1e1d55e11dd
trans <transfire@gmail.com>
2010-08-12 13:52:53 -0400

    add Kernel#temporarily

---
+++ lib/core/facets/kernel/temporarily.rb
@@ -0,0 +1,23 @@
+module Kernel
+
+  # Temporarily set variables while yielding a block, then return the
+  # variables to their original settings when complete.
+  #
+  #   temporarily('$VERBOSE'=>false) do
+  #     $VERBOSE.assert == false
+  #   end
+  #
+  def temporarily(settings) #:yield:
+    cache = {}
+    settings.each do |var, val|
+      cache[var] = eval("var")
+      eval "proc{ |v| #{var} = v }").call(val)
+    end
+    yield
+  ensure
+    cache.each do |var, val|
+      eval "proc{ |v| #{var} = v }").call(val)
+    end
+  end
+
+end
