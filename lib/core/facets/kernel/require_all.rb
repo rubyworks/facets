665b61c6c4cf5f99b89a047b820ccca02b0e2795
trans <transfire@gmail.com>
2010-08-12 21:32:07 -0400

    Kernel#require_all is only for relative requires

--- lib/core/facets/kernel/require_all.rb
+++ lib/core/facets/kernel/require_all.rb
@@ -1,24 +1,40 @@
 module Kernel
 
-  # Require a pattern of files. This make is easy
-  # to require an entire directory, for instance...
+  # Require a pattern of files relatvie to the current file.
+  # This makes is easy to require an entire directory, for instance:
   #
-  #   require_all 'facets/time/*'
+  #   require_all 'core_ext/*'
   #
-  # You can also you #require_all relative to the
-  # current script simply by using __DIR__...
-  #
-  #   require_all(__DIR__ + '/foo/*')
-  #
-  def require_all(pat)
-    $LOAD_PATH.each do |path|
-      fs = Dir[File.join(path,pat)]
-      unless fs.empty?
-        fs.each { |f|
-          Kernel.require(f) unless File.directory?(f)
-        }
-        break;
-      end
+  # NOTE: This method used to allow glob-based requires from the $LOAD_PATH,
+  # but this was deprecated in favor of relative requiring only, as it is
+  # consider the typical usecase, and globbing from the $LOAD_PATH is a
+  # bit dangerous. Better options exist for globbing the $LOAD_PATH such as
+  # the +plugins+ gem.
+
+  def require_all(pattern)
+    c = caller.first
+    fail "Can't parse #{c}" unless c.rindex(/:\d+(:in `.*')?$/)
+    file = $` # File.dirname(c)
+    if /\A\((.*)\)/ =~ file # eval, etc.
+      raise LoadError, "require_relative is called in #{$1}"
+    end
+    glob = File.expand_path(pattern, File.dirname(file))
+    Dir.glob(glob).each do |absolute|
+      require absolute
+    end
+  end
+
+  # Same as #require_all, but for #load.
+  def load_all(pattern, safe=nil)
+    c = caller.first
+    fail "Can't parse #{c}" unless c.rindex(/:\d+(:in `.*')?$/)
+    file = $` # File.dirname(c)
+    if /\A\((.*)\)/ =~ file # eval, etc.
+      raise LoadError, "require_relative is called in #{$1}"
+    end
+    glob = File.expand_path(pattern, File.dirname(file))
+    Dir.glob(glob).each do |absolute|
+      load absolute, safe
     end
   end
 
