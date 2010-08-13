b96962a851b418ad00da733520018032de1fdce4
trans <transfire@gmail.com>
2010-08-12 21:23:15 -0400

    deprecate Kernel#populate and #set_from

--- lib/core/facets/kernel/assign.rb
+++ lib/core/facets/kernel/assign.rb
@@ -45,10 +45,38 @@ module Kernel
     self
   end
 
-  # DEPRECATE: Use #assign instead.
-  def populate(*a,&b)
-    warn 'use #assign instead of #populate for future versions'
-    assign(*a,&b)
+  # DEPRECATED: Use #assign instead.
+  #def populate(*a,&b)
+  #  warn 'use #assign instead of #populate for future versions'
+  #  assign(*a,&b)
+  #end
+
+  # Set attribute writers using like readers from another object.
+  #
+  #   class AssignExample
+  #     attr_accessor :a, :b
+  #     def initialize(a, b)
+  #        @a, @b = a, b
+  #     end
+  #   end
+  #
+  #   obj1 = AssignExample.new(1,2)
+  #   obj2 = AssignExample.new(3,4)
+  #
+  #   obj2.assign_from(obj1, :a, :b)
+  #
+  #   obj2.a  #=> 1
+  #   obj2.b  #=> 2
+  #
+  # TODO: Should this be called #set_from ?
+
+  def assign_from(obj, *fields)
+    fields.each do |k|
+      send("#{k}=", obj.__send__("#{k}"))  #if self.respond_to?("#{k}=") && obj.respond_to?("#{k}")
+    end
   end
+
+  # DEPRECATED: Original name for #assign_from.
+  #alias_method :set_from, :assign_from
 end
 
