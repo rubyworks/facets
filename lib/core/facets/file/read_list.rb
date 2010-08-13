f4426bc2a25567710237194847aed071e0822bb0
trans <transfire@gmail.com>
2010-08-12 21:18:42 -0400

    File#read_list takes options hash for :chomp and :omit

--- lib/core/facets/file/read_list.rb
+++ lib/core/facets/file/read_list.rb
@@ -3,16 +3,31 @@ class File
   # Reads in a file, removes blank lines and removes lines starting
   # with '#' and then returns an array of all the remaining lines.
   #
+  # Thr remark indicator can be overridden via the +:omit:+ option, which
+  # can be a regualar expression or a string that is match against the
+  # start of a line.
+  #
   # CREDIT: Trans
 
-  def self.read_list(filepath, chomp_string='')
-    farr = nil
-    farr = read(filepath).split("\n")
-    farr.collect! { |line|
-      l = line.strip.chomp(chomp_string)
-      (l.empty? or l[0,1] == '#') ? nil : l
-    }
-    farr.compact
+  def self.read_list(filepath, options={})
+    chomp = options[:chomp]
+    omit  = case options[:omit]
+            when Regexp
+              omit
+            when nil
+              /^\s*\#/
+            else
+              /^\s*#{Regexp.escape(omit)}/
+            end
+
+    list = []
+    readlines(filepath).each do |line|
+      line = line.strip.chomp(chomp)
+      next if line.empty?
+      next if omit === line
+      list << line
+    end
+    list
   end
 
 end
