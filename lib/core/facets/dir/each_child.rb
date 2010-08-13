7e2e4710e7011c6a59ca34b22eafb302345a41fe
trans <transfire@gmail.com>
2010-08-12 21:16:18 -0400

    File#each_child takes ignore parameter

--- lib/core/facets/dir/each_child.rb
+++ lib/core/facets/dir/each_child.rb
@@ -1,11 +1,14 @@
 class Dir
 
   # Like #each, except the "." and ".." special files are ignored.
+  # You can use +ignore+ to override '.' and '..' and ignore 
+  # other entries via a exact match or regular expression.
   #
   # CREDIT: Tyler Rick
-  def each_child
+  def each_child(*ignore)
+    ignore = ['.', '..'] if ignore.empty?
     each do |file|
-      yield file if file != "." and file != ".."
+      yield file unless ignore.any?{ |i| i === file }
     end
   end
 
