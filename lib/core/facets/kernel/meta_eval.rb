7f4e03032e0bc9f38afab262699a0c84b3a89fa0
trans <transfire@gmail.com>
2010-08-12 21:33:48 -0400

    Kernel#meta_eval takes file and line arguments

--- lib/core/facets/kernel/meta_eval.rb
+++ lib/core/facets/kernel/meta_eval.rb
@@ -1,16 +1,15 @@
 module Kernel
 
-
   # Evaluate code in a metaclass. This is equivalent to
   # 'meta_class.instance_eval'.
   #
   # CREDIT: WhyTheLuckyStiff
 
-  def meta_eval(str=nil, &blk)
+  def meta_eval(str=nil, *file_and_line, &blk)
     if str
-      meta_class.instance_eval(str)
+      meta_class.instance_eval(str, *file_and_line)
     else
-      meta_class.instance_eval(&blk)
+      meta_class.instance_eval(*file_and_line, &blk)
     end
   end
 
