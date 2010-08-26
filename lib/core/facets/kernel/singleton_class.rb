a0bfea3aa9f75ee8988d0c3eb5df5781282f4b09
trans <transfire@gmail.com>
2010-08-26 15:05:09 -0400

    doc: remove not about singleton_class block support

--- lib/core/facets/kernel/singleton_class.rb
+++ lib/core/facets/kernel/singleton_class.rb
@@ -1,14 +1,9 @@
 module Kernel
 
   # Easy access to an object's "special" class.
-  #
-  # NOTE: This used to support a class eval block,
-  # but to comply with Ruby 1.9.2's definition
-  # it has been removed.
 
   def singleton_class(&block)
     (class << self; self; end)
-  end unless method_defined? :singleton_class
+  end unless method_defined?(:singleton_class)
 
 end
-
