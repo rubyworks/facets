1809506419002706e2f6b18d7fb82b2068b0598c
trans <transfire@gmail.com>
2010-08-26 09:24:29 -0400

    renamed Class#prepend to Class#preallocate

--- lib/tour/facets/class/preallocate.rb
+++ lib/tour/facets/class/preallocate.rb
@@ -1,56 +1,48 @@
-# :title:       Prepend
-# :copyright:   Copyright (c) 2005 Thomas Sawyer
-# :license:     Ruby License
-# :description: Prepend a module to another module, or to a class.
-
-# = Prepend
-#
-# This is a module prepend system, which provides an elegant
-# way to prepend code to the class hierarchy rather then append
-# it (a la #include).
-#
-#   class C
-#     def f
-#       "f"
-#     end
-#   end
-#
-#   module M
-#     def f
-#       '{' + super + '}'
-#     end
-#   end
-#
-#   class C
-#     prepend M
-#   end
-#
-#   c = C.new
-#   c.f  #=> "{f}"
-#
-# This works by overriding Class#new so that all prepended modules
-# extend new instances of the class upon instantiation.
-#
-# If needed the original #new method has been aliased as #init.
-
 class Class
 
+  # Designate aspect modules to be added to a object at instantiation.
   #
-  def prepend(*mods)
-    @prepend ||= []
-    @prepend.concat(mods)
-    @prepend
-  end
+  #   class Firetruck
+  #     def put_out_fire(option)
+  #       "Put out #{option}"
+  #     end
+  #   end
+  #
+  #   module FastFiretruck
+  #     def put_out_fire(option)
+  #       super("very #{option}!")
+  #     end
+  #   end
+  #
+  #   Firetruck.preallocate(FastFiretruck)
+  #
+  #   ft = Firetruck.new
+  #   ft.put_out_fire('fast') #=> "Put out very fast!"
+  #
+  # This method is very similar to the idea of #prepend, but it has some
+  # limitations in that it works by overriding #new and #allocate and extends
+  # an object with the aspect modules on instantiation. A true #prepend
+  # implementation would not have to do this --but would be a natural part
+  # of the class heirarchy instead. For this reason, this method has been
+  # named #preallocate, rather than #prepend.
+  #
+  # CREDIT: Trans
 
-  alias_method :init, :new
-
-  def new(*args, &blk)
-    o = allocate
-    prepend.each do |mod|
-      o.extend(mod)
+  def preallocate(aspect)
+    _new      = method(:new)
+    _allocate = method(:allocate)
+    (class << self; self; end).class_eval do
+      define_method(:new) do |*args|
+        o = _new.call(*args)
+        o.extend aspect
+        o
+      end
+      define_method(:allocate) do |*args|
+        o = _allocate.call(*args)
+        o.extend aspect
+        o
+      end
     end
-    o.__send__(:initialize, *args, &blk) #if private_method_defined?(:initialize)
-    o
   end
 
 end
