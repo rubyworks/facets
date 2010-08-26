1abf8e49c24bc786631757cda6682a29aebd568f
trans <transfire@gmail.com>
2010-08-26 08:45:43 -0400

    Module#class_extend wraps #append_features dynamically

--- lib/tour/facets/module/class_extend.rb
+++ lib/tour/facets/module/class_extend.rb
@@ -1,57 +1,4 @@
-# = Class Extension
-#
-# Normally when including modules, class/module methods are not
-# extended. To achieve this behavior requires some clever
-# Ruby Karate. Instead class_extension provides an easy to use
-# and clean solution. Simply place the extending class methods
-# in a block of the special module method #class_extension.
-#
-#   module Mix
-#     def inst_meth
-#       puts 'inst_meth'
-#     end
-#
-#     class_extend do
-#       def class_meth
-#         "Class Method!"
-#       end
-#     end
-#   end
-#
-#   class X
-#     include Mix
-#   end
-#
-#   X.class_meth  #=> "Class Method!"
-#
-# == History
-#
-# Thanks to Trans, Nobu and Ulysses for their original on this concept.
-#
-# == Authors
-#
-# * Daniel Schierbeck
-# * Thomas Sawyer
-# * Nobu Nakada
-# * Ulysses
-#
-# == Copying
-#
-# Copyright (c) 2006 Daniel Schierbeck
-#
-# Ruby License
-#
-# This module is free software. You may use, modify, and/or redistribute this
-# software under the same terms as Ruby.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.
-
-#
 class Module
-
-  alias_method :append_features_without_class_extension, :append_features
 
   # Normally when including modules, class/module methods are not
   # extended. To achieve this behavior requires some clever
@@ -82,44 +29,54 @@ class Module
   # to be done by hand. With #class_extend, that is no longer
   # the case.
   #
+  # CREDIT: Daniel Schierbeck, Thomas Sawyer
+  #
+  # THANKS: Nobu Nakada, Ulysses
+
   def class_extend(*mods, &block)
-    @class_extension ||= Module.new do
-      def self.append_features(mod)
-        append_features_without_class_extension(mod)
+    class_extension = Module.new
+
+    class_extension.__send__(:include, *mods)
+    class_extension.module_eval(&block) if block_given?
+
+    extend(class_extension)  # extend this module too
+
+    append_method = method(:append_features)
+
+    (class << self; self; end).class_eval do
+      define_method(:append_features) do |mod|
+        append_method.call(mod)
+        mod.extend(class_extension)
+        if mod.instance_of?(Module)
+          mod.__send__(:class_extend, class_extension)
+        end
       end
     end
-    @class_extension.__send__(:include, *mods)
-    @class_extension.module_eval(&block) if block_given?
-    extend(@class_extension)  # extend this module too
-    @class_extension
-  end
 
-  # TODO: DEPRECATE
-  alias_method :class_extension, :class_extend
+    class_extensions << class_extension
+  end
 
   #private :class_extend
 
-  # Override +append_features+ to handle class-inheritable extensions.
-  def append_features(mod)
-    append_features_without_class_extension(mod)
-    mod.extend(class_extend)
-    if mod.instance_of? Module
-      mod.__send__(:class_extend).__send__(:include, class_extend)
-    end
+  #
+  def class_extensions
+    @class_extensions ||= []
   end
 
 end
 
 class Class
-  # For Class, #class_extend is similar to class_eval.
+
+  # For Class, Module#class_extend is similar to class_eval.
+  #
   # The alternative is to "undef_method :class_extend",
   # but this seems uneccessarily limited.
   def class_extend(*mods, &block)
-    m = Module.new
-    m.__send__(:include, *mods)
-    m.module_eval(&block) if block
-    extend(m)
-    m
+    class_extension = Module.new
+    class_extension.__send__(:include, *mods)
+    class_extension.module_eval(&block) if block
+    extend(class_extension)
+    class_extensions << class_extension
   end
-end
 
+end
