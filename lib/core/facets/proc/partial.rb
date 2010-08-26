5c1e15daf3f48de7ae7f11d3db5415e3c2b90b08
trans <transfire@gmail.com>
2010-08-26 08:02:07 -0400

    Proc#partial uses NA for slots, not #__

--- lib/core/facets/proc/partial.rb
+++ lib/core/facets/proc/partial.rb
@@ -1,14 +1,21 @@
+require 'facets/na'
+
 class Proc
 
   # Convert a Proc object into new partial Proc object.
   #
   #   a = proc { |a,b,c| a+b+c }
-  #   b = a.partial(X,2,X)
+  #   b = a.partial(NA,2,NA)
   #   b[1,3] #=> 6
   #
+  # Note, the #__ method, which used to be used in stay of NA,
+  # has been deprecated.
+  #
+  #--
   #   a = proc { |a,b,c| a+b+c }
   #   b = a.partial(__,2,__)
   #   b[1,3] #=> 6
+  #++
   #
   # This method is similar to Proc#curry.
   #
@@ -17,23 +24,11 @@ class Proc
   def partial(*args)
     Proc.new do |*spice|
       result = args.collect do |a|
-        X == a ? spice.pop : a
+        NA == a ? spice.pop : a
       end
       call(*result)
     end
   end
 
-end
-
-# X class is used to represent an argument "slot".
-
-class X < ArgumentError #:nodoc:
-end
-
-# Convenience method for an argument slot.
-# It simple returns the X exception class.
-
-def __
-  X
 end
 
