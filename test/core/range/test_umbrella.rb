ac6effdcff18b019285750c99d97cbcc9b62a838
trans <transfire@gmail.com>
2010-08-27 10:42:01 -0400

    test: split Range#umbrella test out from #within test

---
+++ test/core/range/test_umbrella.rb
@@ -0,0 +1,28 @@
+Covers 'facets/range/umbrella'
+
+Case Range do
+
+  Unit :umbrella do
+    (3..6).umbrella(3..6).assert == [0,0]
+    (3...6).umbrella(3...6).assert == [0,0]
+  end
+
+  Unit :umbrella => "partially aligned" do
+    (3..6).umbrella(2..6).assert == [1,0]
+    (3..6).umbrella(3..7).assert == [0,1]
+    (3..6).umbrella(4..6).assert == [-1,0]
+    (3..6).umbrella(3..5).assert == [0,-1]
+  end
+
+  Unit :umbrella => "offset" do
+    (3..6).umbrella(2..7).assert == [1,1]
+    (3..6).umbrella(4..7).assert == [-1,1]
+    (3..6).umbrella(2..5).assert == [1,-1]
+    (3..6).umbrella(4..5).assert == [-1,-1]
+  end
+
+  Unit :umbrella => "offset by exclusion" do
+    (10...20).umbrella(10..20).assert == [0,1]
+  end
+
+end
