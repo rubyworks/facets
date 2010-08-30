a1fabf41dec720ac5e4a85ddb2080111664c7f8d
trans <transfire@gmail.com>
2010-08-30 14:17:53 -0400

    test: fix tests for Array#rotate

--- test/core/array/test_rotate.rb
+++ test/core/array/test_rotate.rb
@@ -5,25 +5,25 @@ testcase Array do
   unit :rotate => "clockwise" do
     a = [1,2,3]
     r = a.rotate
-    r.assert == [3,1,2]
+    r.assert == [2,3,1]
   end
 
   unit :rotate => "counter-clockwise" do
     a = [1,2,3]
     r = a.rotate(-1)
-    r.assert == [2,3,1]
+    r.assert == [3,1,2]
   end
 
   unit :rotate! => "clockwise" do
     a = [1,2,3]
     a.rotate!
-    a.assert == [3,1,2]
+    a.assert == [2,3,1]
   end
 
   unit :rotate! => "counter-clockwise" do
     a = [1,2,3]
     a.rotate!(-1)
-    a.assert == [2,3,1]
+    a.assert == [3,1,2]
   end
 
 end
