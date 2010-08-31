d5f676f8c878acbb769a42af76d304f232fdf0b3
trans <transfire@gmail.com>
2010-08-31 11:56:12 -0400

    convert method names to symbols to work in 1.8 and 1.9

--- lib/core/facets/module/instance_method_defined.rb
+++ lib/core/facets/module/instance_method_defined.rb
@@ -5,7 +5,7 @@ class Module
   # CREDIT: Gavin Sinclair, Noah Gibbs
 
   def instance_method_defined?(meth)
-    instance_methods(true).find{ |m| m == meth.to_s }
+    instance_methods(true).find{ |m| m.to_sym == meth.to_sym }
   end
 
   # Query whether a normal (singleton) method is defined for the module.
@@ -13,11 +13,11 @@ class Module
   # CREDIT: Gavin Sinclair, Noah Gibbs
 
   def singleton_method_defined?(meth)
-    singleton_methods(true).find{ |m| m == meth.to_s }
+    singleton_methods(true).find{ |m| m.to_sym == meth.to_sym }
   end
 
   #
   alias_method :module_method_defined?, :singleton_method_defined?
+  alias_method :class_method_defined?,  :singleton_method_defined?
 
 end
-
