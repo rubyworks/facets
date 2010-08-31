5087fd6d6275274279e7d073be6f78a2681d7e5e
trans <transfire@gmail.com>
2010-08-31 11:55:12 -0400

    Kernel#Object_hexid differs for 1.8 and 1.9

--- lib/core/facets/kernel/object_hexid.rb
+++ lib/core/facets/kernel/object_hexid.rb
@@ -1,13 +1,29 @@
 module Kernel
 
-  # Returns the object id as a string in hexideciaml,
-  # which is how Ruby reports them with inspect...
-  #
-  #   "ABC".object_hexid  #=> "0x402d359c"
-  #
-  def object_hexid
-    "0x" << (__id__ << 1).to_s(16)
+  if RUBY_VERSION < '1.9'
+
+    # Returns the object id as a string in hexideciaml,
+    # which is how Ruby reports them with inspect...
+    #
+    #   "ABC".object_hexid  #=> "0x402d359c"
+    #
+    def object_hexid
+      hex = (__id__ << 1) #.to_s(16)
+      "0x%x" % hex
+    end
+
+  else
+
+    # Returns the object id as a string in hexideciaml,
+    # which is how Ruby reports them with inspect...
+    #
+    #   "ABC".object_hexid  #=> "0x402d359c"
+    #
+    def object_hexid
+      hex = (__id__ << 1)
+      "0x%014x" % hex
+    end
+
   end
 
 end
-
