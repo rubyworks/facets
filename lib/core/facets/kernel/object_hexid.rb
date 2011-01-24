217272673e2511a5989f1f9fc151b6fe498c10cf
trans <transfire@gmail.com>
2011-01-24 11:34:19 -0500

    minor: simplify object_hexid until real solution found

--- lib/core/facets/kernel/object_hexid.rb
+++ lib/core/facets/kernel/object_hexid.rb
@@ -1,29 +1,13 @@
 module Kernel
 
-  if RUBY_VERSION < '1.9'
-
-    # Returns the object id as a string in hexideciaml,
-    # which is how Ruby reports them with inspect...
-    #
-    #   "ABC".object_hexid  #=> "0x402d359c"
-    #
-    def object_hexid
-      hex = (__id__ << 1) #.to_s(16)
-      "0x%x" % hex
-    end
-
-  else
-
-    # Returns the object id as a string in hexideciaml,
-    # which is how Ruby reports them with inspect...
-    #
-    #   "ABC".object_hexid  #=> "0x402d359c"
-    #
-    def object_hexid
-      hex = (__id__ << 1)
-      "0x%014x" % hex
-    end
-
+  # Returns the object id as a string in hexideciaml,
+  # which is how Ruby reports them with inspect...
+  #
+  #   "ABC".object_hexid  #=> "0x402d359c"
+  #
+  def object_hexid
+    hex = (__id__ << 1)
+    "0x%x" % hex
   end
 
 end
