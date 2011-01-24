3824bf277c9c4e346cc965bd12be043a844501f1
trans <transfire@gmail.com>
2011-01-24 10:29:45 -0500

    improve Platform class, better OS detection and CPU byte order

--- lib/more/facets/platform.rb
+++ lib/more/facets/platform.rb
@@ -1,9 +1,12 @@
-# Original version from RubyGems, Copyright (c) 2007 Eric Hodel
+require 'facets/rbconfig'
 
-require 'rbconfig'
+unless defined?(RUBY_ENGINE)
+  RUBY_ENGINE = 'ruby'
+end
 
 # Platform class
 #
+# Original version from RubyGems, Copyright (c) 2007 Eric Hodel
 class Platform
 
   # A pure-ruby gem that may use Gem::Specification#extensions to build
@@ -197,15 +200,20 @@ class Platform
     self === other
   end
 
-  # Byte order of CPU.
+  # B Y T E  O R D E R
+
+  # Byte order of CPU. Determine byte order of underlying machine.
+  #
+  # Examines the byte order of the underlying machine. Returns <code>:big</code>
+  # if Big Endian and <code>:little</code> if Little Endian.
   #
   # TODO: This doesn't work well apearently. We need to use a chart for possible
   # cpus instead, maybe.
   def byte_order
     if [0x12345678].pack("L") == "\x12\x34\x56\x78"
-      BigEndian
+      :big
     else
-      LittleEndian
+      :little
     end
   end
 
@@ -222,5 +230,148 @@ class Platform
     byte_order == :big
   end
 
+  # R U B Y  E N G I N E
+
+  # Return the name of the ruby interpreter.
+  def interpreter
+    @interpreter ||= (
+      if RUBY_PLATFORM == 'parrot'
+        'cardinal'
+      elsif RUBY_ENGINE == 'ruby'
+        if RUBY_DESCRIPTION =~ /Enterprise/i
+          'ree'
+        else
+          'mri'
+        end
+      else
+        RUBY_ENGINE.to_s # jruby, rbx, ironruby, macruby, etc.
+      end
+    )
+  end
+
+  def mri?
+    'mri' === interpreter
+  end
+  alias official_ruby? mri?
+  alias ruby? mri?
+
+  def jruby?
+    'jruby' === interpreter
+  end
+  alias java? jruby?
+
+  def rubinius?
+    'rbx' === interpreter
+  end
+  alias rbx? rubinius?
+
+  def ree?
+    'ree' === interpreter
+  end
+  alias enterprise? ree?
+
+  def ironruby?
+    'ironruby' === interpreter
+  end
+  alias iron_ruby? ironruby?
+
+  def cardinal?
+    'cardinal' === interpreter
+  end
+  alias parrot? cardinal?
+  alias perl? cardinal?
+
+
+  # O P E R A T I N G  S Y S T E M
+
+  def host_os
+    RbConfig::CONFIG['host_os']
+  end
+
+  def linux?
+    host_os =~ /linux|cygwin/
+  end
+
+  def mac?
+    host_os =~ /mac|darwin/
+  end
+
+  def bsd?
+    host_os =~ /bsd/
+  end
+
+  def windows?
+    host_os =~ /mswin|mingw/
+  end
+
+  def solaris?
+    host_os =~ /solaris|sunos/
+  end
+
+  # TODO: who knows what symbian returns?
+  def symbian?
+    host_os =~ /symbian/
+  end
+
+  #
+  def posix?
+    linux? or mac? or bsd? or solaris? or begin 
+      fork do end
+      true
+    rescue NotImplementedError, NoMethodError
+      false
+    end
+  end
+
 end
+
+
+# Current CPU's Byte Order
+#BYTE_ORDER = RbConfig.byte_order
+
+# Current CPU
+#CPU = RbConfig.platform.cpu
+
+# Current Operating System
+#OS  = RbConfig.platform.os
+
+# Current Operating System Version
+#OS_VERSION = RbConfig.platform.version
+
+
+=begin
+  # platform
+  def current_platform
+    arch = Config::CONFIG['arch']
+    #cpu, os = arch.split '-', 2
+    return match_platform(arch)
+  end
+
+  #
+  def match_platform(arch)
+    cpu, os = arch.split '-', 2
+    cpu, os = nil, cpu if os.nil? # java
+
+    cpu = case cpu
+          when /i\d86/ then 'x86'
+          else cpu
+          end
+
+    os  = case os
+          when /cygwin/ then            [ 'cygwin',  nil ]
+          when /darwin(\d+)?/ then      [ 'darwin',  $1  ]
+          when /freebsd(\d+)/ then      [ 'freebsd', $1  ]
+          when /^java$/ then            [ 'java',    nil ]
+          when /^java([\d.]*)/ then     [ 'java',    $1  ]
+          when /linux/ then             [ 'linux',   $1  ]
+          when /mingw32/ then           [ 'mingw32', nil ]
+          when /mswin32/ then           [ 'mswin32', nil ]
+          when /openbsd(\d+\.\d+)/ then [ 'openbsd', $1  ]
+          when /solaris(\d+\.\d+)/ then [ 'solaris', $1  ]
+          else                          [ 'unknown', nil ]
+          end
+
+    [cpu, os].flatten.compact.join("-")
+  end
+=end
 
