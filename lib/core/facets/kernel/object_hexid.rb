18781e2000e330976dcaa762c8cbf18dd06f6bbc
trans <transfire@gmail.com>
2011-02-02 10:04:49 -0500

    bugfix: object_hexid varies based on cpu size (32 vs 64)

--- lib/core/facets/kernel/object_hexid.rb
+++ lib/core/facets/kernel/object_hexid.rb
@@ -1,13 +1,31 @@
 module Kernel
 
-  # Returns the object id as a string in hexideciaml,
-  # which is how Ruby reports them with inspect...
-  #
-  #   "ABC".object_hexid  #=> "0x402d359c"
-  #
-  def object_hexid
-    hex = (__id__ << 1)
-    "0x%x" % hex
+  arch_size = ['a'].pack('P').length > 4 ? 64 : 32
+
+  if arch_size = 32  #RUBY_VERSION < '1.9'
+
+    # Returns the object id as a string in hexideciaml,
+    # which is how Ruby reports them with inspect...
+    #
+    #   "ABC".object_hexid  #=> "0x402d359c"
+    #
+    def object_hexid
+      hex = (__id__ << 1) #.to_s(16)
+      "0x%x" % hex
+    end
+
+  else
+
+    # Returns the object id as a string in hexideciaml,
+    # which is how Ruby reports them with inspect...
+    #
+    #   "ABC".object_hexid  #=> "0x402d359c"
+    #
+    def object_hexid
+      hex = (__id__ << 1)
+      "0x%014x" % hex
+    end
+
   end
 
 end
