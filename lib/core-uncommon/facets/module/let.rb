364ee3b1af797dc1bffed72c8b07c3cc075f7f6f
trans <transfire@gmail.com>
2011-07-17 14:47:02 -0400

    Add Module#let, akin to RSpec's method.

---
+++ lib/core-uncommon/facets/module/let.rb
@@ -0,0 +1,27 @@
+class Module
+
+  # Create a memoized method. This method has been popularized by RSpec.
+  #
+  #   class LetExample
+  #     let(:seed) { rand }
+  #   end
+  #
+  #   eg = LetExample.new
+  #   eg.seed == eg.seed
+  #
+  # CREDIT: Howard Yeh
+  def let(var,&block)
+    name = "@#{var}"
+    self.class_eval do
+      define_method(var) do
+        if instance_variable_defined?(name)
+          instance_variable_get(name)
+        else
+          val = self.instance_eval(&block)
+          instance_variable_set(name,val)
+        end
+      end
+    end
+  end
+
+end
