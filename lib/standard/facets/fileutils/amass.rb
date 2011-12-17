12e5d04a0cab713a531ff79ff5c51d6f44e6db63
trans <transfire@gmail.com>
2011-12-17 13:41:50 -0500

    Make FileUtils#amass more robust.

--- lib/standard/facets/fileutils/amass.rb
+++ lib/standard/facets/fileutils/amass.rb
@@ -2,7 +2,7 @@ module FileUtils
 
   module_function
 
-  # An intergrated glob like method that take a set of include globs,
+  # An intergrated glob like method that takes a set of include globs,
   # exclude globs and ignore globs to produce a collection of paths.
   # 
   # The ignore_globs differ from exclude_globs in that they match by
@@ -11,18 +11,19 @@ module FileUtils
   # TODO: Should ignore be based on any portion of the path, not just the basename?
   #
   def amass(include_globs, exclude_globs=[], ignore=[])
-    include_files = include_globs.flatten.map{ |g| Dir.glob(g) }.flatten.uniq
-    exclude_files = exclude_globs.flatten.map{ |g| Dir.glob(g) }.flatten.uniq
 
-    include_globs = include_globs.map{ |f| File.directory?(f) ? File.join(f, '**/*') : f } # Recursive!
-    exclude_globs = exclude_globs.map{ |f| File.directory?(f) ? File.join(f, '**/*') : f } # Recursive!
+    include_files = [include_globs].flatten.map{ |g| Dir.glob(g) }.flatten.uniq
+    exclude_files = [exclude_globs].flatten.map{ |g| Dir.glob(g) }.flatten.uniq
 
-    include_files = include_globs.flatten.map{ |g| Dir.glob(g) }.flatten.uniq
-    exclude_files = exclude_globs.flatten.map{ |g| Dir.glob(g) }.flatten.uniq
+    include_files = include_files.map{ |f| File.directory?(f) ? File.join(f, '**/*') : f } # Recursive!
+    exclude_files = exclude_files.map{ |f| File.directory?(f) ? File.join(f, '**/*') : f } # Recursive!
+
+    include_files = include_files.flatten.map{ |g| Dir.glob(g) }.flatten.uniq
+    exclude_files = exclude_files.flatten.map{ |g| Dir.glob(g) }.flatten.uniq
 
     files = include_files - exclude_files
 
-    files = files.reject{ |f| ignore.any?{ |x| File.fnmatch?(x, File.basename(f)) } }
+    files = files.reject{ |f| [ignore].flatten.any?{ |x| File.fnmatch?(x, File.basename(f)) } }
 
     files
   end
