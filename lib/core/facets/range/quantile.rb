d25b73893491077f255e0bcdf0882e3c2afee607
trans <transfire@gmail.com>
2011-12-22 08:28:46 -0500

    Add Range#quantile as optional core extension.

---
+++ lib/core/facets/range/quantile.rb
@@ -0,0 +1,15 @@
+class Range
+
+  # Calculate the kth n-tile in a range.
+  #
+  # If n=4 the quantity is called a quartile, and if n=100
+  # it is called a percentile.
+  #
+  # @return [Integer] the kth n-tile
+  def quantile(k, n=100)
+    return 1 if k < first
+    return n if k >= last
+    ((k - first) / ((last - first) / n.to_f)).to_i + 1
+  end
+
+end
