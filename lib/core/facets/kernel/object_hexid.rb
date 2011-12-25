49570c668fa826732705b0cca958c02930059b69
trans <transfire@gmail.com>
2011-12-25 12:51:56 -0500

    Improve #object_hexid once again.

--- lib/core/facets/kernel/object_hexid.rb
+++ lib/core/facets/kernel/object_hexid.rb
@@ -1,31 +1,33 @@
 module Kernel
 
-  arch_size = (['a'].pack('P').length > 4 ? 64 : 32)
+  ARCH_SIZE = (['a'].pack('P').length > 4 ? 64 : 32)
 
-  if arch_size == 64
-
-    # Returns the object id as a string in hexideciaml,
-    # which is how Ruby reports them with inspect...
-    #
-    #   "ABC".object_hexid  #=> "0x402d359c"
-    #
-    def object_hexid
-      hex = (__id__ << 1)
-      "0x%014x" % hex
+  if RUBY_VERSION < '1.9'
+    if ARCH_SIZE == 64
+      HEXID_TEMPLATE = "0x%012x"
+    else
+      HEXID_TEMPLATE = "0x%x"
     end
-
   else
-
-    # Returns the object id as a string in hexideciaml,
-    # which is how Ruby reports them with inspect...
-    #
-    #   "ABC".object_hexid  #=> "0x402d359c"
-    #
-    def object_hexid
-      hex = (__id__ << 1) #.to_s(16)
-      "0x%x" % hex
+    if ARCH_SIZE == 64
+      HEXID_TEMPLATE = "0x%014x"
+    else
+      HEXID_TEMPLATE = "0x%x"
     end
-
   end
+
+  # Returns the object id as a string in hexideciaml,
+  # which is how Ruby reports them with inspect...
+  #
+  #   "ABC".object_hexid  #=> "0x402d359c"
+  #
+  def object_hexid
+    HEXID_TEMPLATE % (__id__ << 1)
+  end
+
+  # The 1.9.3 definition is as follows:
+  #def object_hexid
+  #  "0x" + [object_id << 1].pack("L>!").unpack("H*")[0]
+  #end
 
 end
