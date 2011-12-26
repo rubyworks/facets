d2103e62d49031d1cb2cbce09f322c0cd937df4b
trans <transfire@gmail.com>
2011-12-26 10:10:14 -0500

    Add Enumerable#hashify. (Ronen Barzel)

---
+++ lib/core/facets/enumerable/hashify.rb
@@ -0,0 +1,28 @@
+module Enumerable
+  # Create a hash whose keys are the enumerable's elements, with specified
+  # values.
+  #
+  # If no block is given, the given parameter (default true) is used for
+  # all values, e.g.:
+  #
+  #     [1,2,3,4].hashify               #=> {1=>true, 2=>true, 3=>true, 4=>true}
+  #     [1,2,3,4].hashify("a")          #=> {1=>"a", 2=>"a", 3=>"a", 4=>"a"}
+  #
+  # If a block is given, each key's value is the result of running the
+  # block for that key, e.g.:
+  #
+  #     [1,2,3,4].hashify{|n| "a" * n}  #=> {1=>"a", 2=>"aa", 3=>"aaa", 4=>"aaaa"}
+  #
+  # @credit Ronen Barzel
+
+  def hashify(val=true)
+    h = {}
+    if block_given?
+      each { |item| h[item] = yield(item) }
+    else
+      each { |item| h[item] = val }
+    end
+    h
+  end
+
+end
