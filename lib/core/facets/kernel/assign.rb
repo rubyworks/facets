e57c18efc68f0d17de63a556d96c35f8f578d470
trans <transfire@gmail.com>
2011-12-26 13:51:44 -0500

    Use NA instead of Exception for #assign argument default.

--- lib/core/facets/kernel/assign.rb
+++ lib/core/facets/kernel/assign.rb
@@ -1,3 +1,5 @@
+require 'facets/na'
+
 module Kernel
 
   # Assign via writer using arguments, hash or associative array.
@@ -22,25 +24,21 @@ module Kernel
   #   object.a = 1 if object.respond_to?(:a=)
   #   object.b = 2 if object.respond_to?(:b=)
   #
-  # Using an associative array instead of hash guarantees order of assignemnt
+  # Using an associative array instead of a hash guarantees order of assignment
   # for older versions of Ruby (< 1.8.7).
-  #
-  # NOTE: This method used to allow a block which would yield on +self+.
-  # This feature has been deprecated. There are plenty of other ways to
-  # handle this, such as #tap.
   #
   # TODO: Should this be called #set instead? Consider Module#set in this
   # question, and also #set_from as the alias of #assign_from.
 
-  def assign(data=nil, value=Exception) #:yield:
-    if data
-      if value==Exception
-        data.each do |(k,v)|
-          __send__("#{k}=", v) if respond_to?("#{k}=")
-        end
-      else
-        __send__("#{data}=", value) if respond_to?("#{data}=")
+  def assign(data=nil, value=NA)
+    return self unless data
+
+    if value==NA
+      data.each do |(k,v)|
+        __send__("#{k}=", v) if respond_to?("#{k}=")
       end
+    else
+      __send__("#{data}=", value) if respond_to?("#{data}=")
     end
     self
   end
