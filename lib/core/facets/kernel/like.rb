b9b715fa62c14a68329f825de6ab8f2085eeb7fa
trans <transfire@gmail.com>
2012-03-30 21:29:01 -0400

    Rename Kernel#equate? method to #like?.

--- lib/core/facets/kernel/like.rb
+++ lib/core/facets/kernel/like.rb
@@ -4,13 +4,16 @@ module Kernel
   # way equal to the reciever, starting with the identity
   # #equal?, then #eql?, then #==, and ending with #===.
   #
-  #   1.equate?(1.0)               #=> true
-  #   "string".equate?("string")   #=> true
-  #   String.equate?("string")     #=> true
+  #   1.like?(1.0)               #=> true
+  #   "string".likee?("string")   #=> true
+  #   String.like?("string")     #=> true
   #
-  def equate?(x)
+  def like?(x)
     equal?(x) || eql?(x) || self == x || self === x
   end
+
+  # @deprecated
+  alias :equate? :like?
 
 end
 
