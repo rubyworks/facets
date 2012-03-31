a9ed2f91e5841512a1f92cc59d07cb9a2ce5579f
trans <transfire@gmail.com>
2012-03-30 22:10:01 -0400

    Improve Method#composition to handle multiple functions.

--- lib/core/facets/method/composition.rb
+++ lib/core/facets/method/composition.rb
@@ -1,55 +1,70 @@
 class Method
 
+  # TODO: Is it really necessary to have method composition?
+  # Could we not just convert to Proc and have Proc omposition,
+  # which doesn't require a special class (albeit #owner and #arity
+  # and such are not as well supported).
+
   # Method Composition class acts a proxy for composed methods.
   #
   # @author Mike Burns
   class Composition
 
     #
-    def initialize(f,g)
+    def initialize(*f)
       @f = f
-      @g = g
-    end
-
-    #
-    def *(h)
-      Composition.new(self, h)
-    end
-
-    #
-    def ^(n)
-      return self if n < 2
-      Composition.new(self, self ^ (n-1))
-    end
-
-    #
-    def owner
-      @g.owner
-    end
-
-    #
-    def receiver
-      @g.receiver
     end
 
     #
     def inspect
-      "#<Method::Composition: #{@f.inspect} * #{@g.inspect}>"
+      x = @f.map{ |f| f.inspect }.join(' * ')
+      "#<Method::Composition: #{x}>"
+    end
+
+    #
+    def *(h)
+      #Composition.new(self, h)
+      @f << h
+    end
+
+    #
+    def ^(n)
+      raise ArgumentError if n < 0
+
+      return self if n < 2
+
+      #Composition.new(self, self ^ (n-1))
+      (n - 1).times{ @f = @f.concat(@f) }
+    end
+
+    #
+    def owner
+      @f.last.owner
+    end
+
+    #
+    def receiver
+      @f.first.receiver
     end
 
     #
     def arity
-      @g.arity
+      @f.last.arity
     end
 
     #
     def to_proc
-      Proc.new {|x| @f.call(*@g.call(*x)) }
+      #Proc.new {|x| @f.call(*@g.call(*x)) }
+      Proc.new { |*x| call(*x) }
     end
 
     #
-    def call(x)
-      @f.call(*@g.call(*x))
+    def call(*x)
+      r = x
+      @f.reverse_each do |f|
+        r = f.call(*r)
+      end
+      r
     end
 
     #
@@ -73,7 +88,8 @@ class Method
     if n < 2
       self
     else
-      Composistion.new(self, self ^ (n-1))
+      #Composition.new(self, self ^ (n-1))
+      Composition.new( *([self] * n) )
     end
   end
 
