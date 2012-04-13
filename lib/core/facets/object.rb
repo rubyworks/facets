88b584a3a5f1e484b71e0eaf7f55cfc021931ddc
Michael Kohl <citizen428@gmail.com>
2012-04-13 21:22:20 +0200

    Object#identity

--- lib/core/facets/object.rb
+++ lib/core/facets/object.rb
@@ -2,7 +2,7 @@ require 'facets/kernel/require_relative'
 
 require_relative 'object/clone.rb'
 require_relative 'object/dup.rb'
+require_relative 'object/identity.rb'
 require_relative 'object/object_state.rb'
 require_relative 'object/replace.rb'
 require_relative 'object/try_dup.rb'
-
