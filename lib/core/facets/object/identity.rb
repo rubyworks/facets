c9e508d41ebe8728be42fbe69367042ea51a06b5
Michael Kohl <citizen428@gmail.com>
2012-04-13 21:22:20 +0200

    Object#identity

---
+++ lib/core/facets/object/identity.rb
@@ -0,0 +1,13 @@
+class Object
+  # An identity method that provides access to an object's 'self'.
+  #
+  # Example:
+  #   [1,2,3,4,5,1,2,2,3].group_by(&:identity)
+  #   #=> {1=>[1, 1], 2=>[2, 2, 2], 3=>[3, 3], 4=>[4], 5=>[5]}
+  #
+  # CREDIT: Michael Kohl
+  #
+  def identity
+    self
+  end
+end
