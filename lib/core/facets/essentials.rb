ef99cd24694a2d9d70f72208735c06d162bfb05b
trans <transfire@gmail.com>
2012-04-27 17:00:06 -0400

    Add facets/essentials for loading most common extensions.

---
+++ lib/core/facets/essentials.rb
@@ -0,0 +1,8 @@
+# As suggested by Roger Pack this script is a simple convenience script
+# for loading only the more "essential" of Facet's core extensions.
+
+require 'facets/to_hash'
+require 'facets/file/write'
+require 'facets/hash/rekey'
+require 'facets/string/tab'
+
