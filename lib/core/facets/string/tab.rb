1aa621e7d11f3d6c827b1e797b3011db420f3c0c
trans <transfire@gmail.com>
2012-04-27 17:22:18 -0400

    Change String#tab to be like #tabto instead of #indent.

--- lib/core/facets/string/tab.rb
+++ lib/core/facets/string/tab.rb
@@ -2,22 +2,36 @@ require 'facets/string/indent'
 
 class String
 
-  # Preserves relative tabbing. The first non-empty line ends up with `n`
-  # spaces before nonspace.
+  # Preserves relative tabbing such that the line with the least amount 
+  # of white space ends up with `n` number of spaces before non-space and
+  # all other lines move relative to it.
+
+  def tab(n)
+    str = gsub("\t", "  ")  # option?
+
+    min = []
+    str.each_line do |line|
+      next if line.strip.empty?
+      min << line.index(/\S/)
+    end
+    min = min.min
+
+    str.indent(n - min)
+  end
+
+  # Preserves relative tabbing, such that the first non-empty line ends up with
+  # `n` number of spaces before non-space, and all subsequent lines move relative
+  # to the first.
   #
   # @author Gavin Sinclair
 
-  def tab(n)
+  def tabto(n)
     if self =~ /^( *)\S/
       indent(n - $1.length)
     else
       self
     end
   end
-
-  # @deprecated Use String#tab instead.
-
-  alias :tabto, :tab
 
 end
 
