b332437af10a39b9e0dce1d3574f4491c38b4b86
trans <transfire@gmail.com>
2012-04-27 20:31:43 -0400

    Fix Symbol#thrown method. [bug]

--- lib/core/facets/symbol/thrown.rb
+++ lib/core/facets/symbol/thrown.rb
@@ -1,26 +1,22 @@
 class Symbol
 
-  # Does a given block throw the symbol?
-  # 
-  # Note that +throw+ inside the block must be used in one-argument form. Also,
-  # note that this definition is not 100% perfect in that it can cause a false
-  # positive if an ArgumentError is thrown by the block and the error message
-  # contains the text `uncaught throw`, even though it is not actually the
-  # error. That, of course, is not likely to ever happen, so it's Good Enough(TM).
+  # Returns truew if the given block throws the symbol, otherwise false.
+  # Note that +throw+ inside the block must be used in one-argument form.
   #
-  # Ideally Ruby would provide a more specific error message (See http://bugs.ruby-lang.org/issues/6372)
-  #
+  # @return [true,false]
   def thrown?
+    thrown = true
     catch(self) do
       begin
         yield
-        true
+        thrown = false
       rescue ArgumentError => err     # 1.9 exception
-        false if err.message.index('uncaught throw')
+        thrown = false if err.message.index('uncaught throw')
       rescue NameError => err         # 1.8 exception
-        false if err.message.index('uncaught throw')
+        thrown = false if err.message.index('uncaught throw')
       end
     end
+    thrown
   end
 
 end
--- test/core/symbol/test_thrown.rb
+++ test/core/symbol/test_thrown.rb
@@ -3,11 +3,16 @@ covers 'facets/symbol/thrown'
 test_case Symbol do
 
   method :thrown? do
-    test do
+
+    test "throws what is expected" do
       assert( :a.thrown? { throw :a } )
     end
 
-    test do
+    test "throws something else" do
+      refute( :a.thrown? { throw :b } )
+    end
+
+    test "throws nothing at all" do
       refute( :a.thrown? { } )
       refute( :a.thrown? { true } )  # to exclude block's return value usage.
     end
