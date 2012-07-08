e9b42bf5085cbed064a89b4cf7a733d4d877f1a2
trans <transfire@gmail.com>
2012-07-08 11:16:41 -0400

    Add Enumerable#pair and alias #associate. [major]

---
+++ lib/core/facets/enumerable/associate.rb
@@ -0,0 +1,6 @@
+require 'facets/enumerable/pair'
+
+module Enumerable
+  # Alias for #pair.
+  alias :associate :pair
+end
---
+++ lib/core/facets/enumerable/pair.rb
@@ -0,0 +1,20 @@
+module Enumerable
+
+  # Like `each_slice(2)` but ensures the last 
+  # element has a pair if odd sized.
+  #
+  #   [:a,1,:b,2,:c,3].pair.to_a  #=> [[:a,1],[:b,2],[:c,3]]
+  #
+  def pair(missing=nil)
+    return to_enum(:pair) unless block_given?
+
+    each_slice(2) do|kv|
+      if kv.size == 1
+        yield kv.first, missing
+      else
+        yield *kv
+      end
+    end
+  end
+
+end
