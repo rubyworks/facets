4d77a0de731f62b4e352fdfd73a63c25620790bd
trans <transfire@gmail.com>
2012-07-08 16:16:12 -0400

    Add Array#to_h method. [major]

--- lib/core/facets/array.rb
+++ lib/core/facets/array.rb
@@ -44,25 +44,7 @@ require_relative 'array/splice.rb'
 require_relative 'array/split.rb'
 require_relative 'array/store.rb'
 require_relative 'array/thru.rb'
+require_relative 'array/to_h.rb'
 require_relative 'array/traverse.rb'
 require_relative 'array/uniq_by.rb'
-
-
-
-
-#folder = __FILE__.chomp('.rb')
-
-#target = File.basename(folder)
-
-#loader = \
-#  if RUBY_VERSION < '1.9'
-#    lambda{ |file| require File.join(folder, file) }
-#  else
-#    lambda{ |file| require_relative File.join(target, file) }
-#  end
-
-#files.each do |file|
-#  next unless file.end_with?('.rb')
-#  loader.call(file)
-#end
 
