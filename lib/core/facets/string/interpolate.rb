b204280d87468e66d4aaf6393b4934519a9d2c8f
trans <transfire@gmail.com>
2012-07-28 18:54:04 -0400

    Make comment on uses of String#interpolate. [doc]

--- lib/core/facets/string/interpolate.rb
+++ lib/core/facets/string/interpolate.rb
@@ -1,13 +1,17 @@
 class String
 
-  # Interpolate. Provides a means of extenally using Ruby string
+  # Interpolate provides a means of externally using Ruby string
   # interpolation mechinism.
   #
   #   try = "hello"
   #   str = "\#{try}!!!"
   #   String.interpolate{ str }    #=> "hello!!!"
   #
-  # NOTE: The block neccessary in order to get then binding of the caller.
+  # Note this uses #eval under the hood. We do not recommend that
+  # it serve in place of a good templating system. But, it can be
+  # useful for simple cases.
+  #
+  # The block is neccessary in order to get then binding of the caller.
   #
   # CREDIT: Trans
 
