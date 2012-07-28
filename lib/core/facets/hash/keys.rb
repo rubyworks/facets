50f5076b5f7d02d808caef5ee018641b0c25d719
trans <transfire@gmail.com>
2012-07-28 18:53:25 -0400

    Add file for Hash#only_keys.

--- lib/core/facets/hash/keys.rb
+++ lib/core/facets/hash/keys.rb
@@ -9,12 +9,12 @@ class Hash
   #
   # CREDIT: Trans
 
-  def has_keys?(*check_keys)
+  def keys?(*check_keys)
     unknown_keys = check_keys - self.keys
     return unknown_keys.empty?
   end
 
-  alias_method :keys?, :has_keys?
+  alias_method :has_keys?, :keys?
 
   # Returns true if the hash contains
   # _only_ the given keys, otherwise false.
@@ -25,21 +25,12 @@ class Hash
   #
   # CREDIT: Trans
 
-  def has_only_keys?(*check_keys)
+  def only_keys?(*check_keys)
     unknown_keys = self.keys - check_keys
     return unknown_keys.empty?
   end
 
-  alias_method :only_keys?, :has_only_keys?
-
-  # Each with key is like each_pair but reverses the order
-  # the parameters to [value,key] instead of [key,value].
-  #
-  # CREDIT: Trans
-
-  def each_with_key( &yld )
-    each_pair{ |k,v| yld.call(v,k) }
-  end
+  alias_method :has_only_keys?, :only_keys?
 
 end
 
