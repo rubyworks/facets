718b8a59f03a2eeb15e0b1ce7623c3f65dc47357
trans <transfire@gmail.com>
2012-07-28 18:38:59 -0400

    Rename Module#basename to #lastname.

--- lib/core/facets/module/lastname.rb
+++ lib/core/facets/module/lastname.rb
@@ -8,24 +8,23 @@ class Module
   #   end
   #
   #   BaseName::Example.name       #=> "BaseName::Example"
-  #   BaseName::Example.basename   #=> "Example"
+  #   BaseName::Example.lastname   #=> "Example"
   #
-  # For anonymous modules this will provide a basename
-  # based on Module#inspect.
-  #
-  #   m = Module.new
-  #   m.inspect       # "#<Module:0xb7bb0434>"
-  #   m.basename      # "Module_0xb7bb0434"
+  # Returns nil when module is anonymous.
   #
   # CREDIT: Trans
 
-  def basename
+  def lastname
     if name and not name.empty?
       name.gsub(/^.*::/, '')
     else
       nil #inspect.gsub('#<','').gsub('>','').sub(':', '_')
     end
   end
+
+  # Original name, but conflicts with File.basename, so is
+  # mildly deprecated. 
+  alias basename lastname
 
 end
 
