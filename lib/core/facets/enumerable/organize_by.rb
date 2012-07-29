4b60b7c851a1cb50039f2363cab499d010c01c94
trans <transfire@gmail.com>
2012-07-28 20:46:58 -0400

    Rename Enumerable#cluster_by to #organize_by.

---
+++ lib/core/facets/enumerable/organize_by.rb
@@ -0,0 +1,21 @@
+require 'facets/enumerable/group_by'
+
+module Enumerable
+
+  # Similar to #group_by but returns an array of the groups.
+  # Returned elements are sorted by block.
+  #
+  #    %w{this is a test}.organize_by {|x| x[0]}
+  #    #=> [ ['a'], ['is'], ['this', 'test'] ]
+  #
+  # CREDIT: Erik Veenstra
+
+  def organize_by(&b)
+    group_by(&b).sort.transpose.pop || []   # group_by(&b).values ?
+  end
+
+  # DEPRECATED: Name changed to avoid confusion with #cluster.
+  alias cluster_by organize_by
+
+end
+
