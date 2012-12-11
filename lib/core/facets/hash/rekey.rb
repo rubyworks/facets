d96ec0d700d1d7180ccbb5452e0a926386ec0b32
trans <transfire@gmail.com>
2012-12-11 16:28:15 -0500

    Improve Hash#rekey code, no NA and only argument or block, not both. [minor]

--- lib/core/facets/hash/rekey.rb
+++ lib/core/facets/hash/rekey.rb
@@ -1,15 +1,11 @@
-require 'facets/symbol/to_proc'
-require 'facets/na'
-
 class Hash
-
-  # TODO: Improve Hash#rekey code!!!
 
   # Rekey a hash:
   #
   #   rekey()
   #   rekey(from_key => to_key, ...)
   #   rekey{|from_key| to_key}
+  #   rekey{|from_key, value| to_key}
   #
   # If a key map is given, then the first key is changed to the second key.
   #
@@ -19,8 +15,7 @@ class Hash
   #   foo.rekey('foo'=>'bar')  #=> { :a =>1, :b=>2 }
   #
   # If a block is given, converts all keys in the Hash accroding to the
-  # given block procedure. If the block returns +NA+ for a given key,
-  # then that key will be left intact.
+  # given block procedure.
   #
   #   foo = { :name=>'Gavin', :wife=>:Lisa }
   #   foo.rekey{ |k| k.to_s }  #=>  { "name"=>"Gavin", "wife"=>:Lisa }
@@ -29,49 +24,46 @@ class Hash
   # If no key map or block is given, then all keys are converted
   # to Symbols.
   #
-  # Note that if both a +key_map+ and a block are given, the +key_map+ is 
-  # applied first then the block.
+  # Raises an ArgumentError if both a +key_map+ and a block are given.
+  # If both are needed just call #rekey twice.
+  #
+  # TODO: If `nil` is returned by block should the key be set to `nil` or the orignal key?
   #
   # CREDIT: Trans, Gavin Kistner
 
   def rekey(key_map=nil, &block)
+    raise ArgumentError, "argument or block" if key_map && block
+
     if !(key_map or block)
       block = lambda{|k| k.to_sym}
     end
 
-    key_map ||= {} 
-
-    hash = dup.replace({})  # to keep default_proc
-
-    (keys - key_map.keys).each do |key|
-      hash[key] = self[key]
-    end
-
-    key_map.each do |from, to|
-      hash[to] = self[from] if key?(from)
-    end
-
     if block
-      hash2 = dup.replace({})
-      case block.arity
-      when 2  # TODO: is this condition needed?
-        hash.each do |k, v|
-          nk = block.call(k,v)
-          nk = (NA == nk ? k : nk)
-          hash2[nk] = v
+      hash = dup.clear
+      if block.arity.abs == 1
+        each_pair do |k, v|
+          hash[block[k]] = v     #hash[block[k] || k] = v
         end
       else
-        hash.each do |k, v|
-          nk = block.call(k)
-          nk = (NA == nk ? k : nk)
-          hash2[nk] = v
+        each_pair do |k, v|
+          hash[block[k,v]] = v   #hash[block[k,v] || k] = v
         end
       end
     else
-      hash2 = hash
+      #hash = dup.clear  # to keep default_proc
+      #(keys - key_map.keys).each do |key|
+      #  hash[key] = self[key]
+      #end
+      #key_map.each do |from, to|
+      #  hash[to] = self[from] if key?(from)
+      #end
+      hash = dup  # to keep default_proc
+      key_map.each_pair do |from, to|
+        hash[to] = hash.delete(from) if hash.key?(from)
+      end
     end
 
-    hash2
+    hash
   end
 
   # Synonym for Hash#rekey, but modifies the receiver in place (and returns it).
--- test/core/hash/test_rekey.rb
+++ test/core/hash/test_rekey.rb
@@ -4,7 +4,7 @@ test_case Hash do
 
   method :rekey do
 
-    test "default" do
+    test "default converts all keys to symbols" do
       foo = { "a"=>1, "b"=>2 }
       foo.rekey.assert == { :a=>1, :b=>2 }
       foo.assert == { "a"=>1, "b"=>2 }
@@ -18,6 +18,27 @@ test_case Hash do
       foo[:a].assert == nil
     end
 
+    test "multiple keys" do
+      bar = { :a=>1, :b=>2, :c=>3 }
+      foo = bar.rekey(:a=>:d,:b=>:e)
+      foo[:d].assert == 1
+      foo[:e].assert == 2
+      foo[:c].assert == 3
+      foo[:a].assert == nil
+      foo[:b].assert == nil
+    end
+
+    # TODO: Is using Hash#rekey to exhange keys possible?
+
+    #test "exchange keys via dummy key" do
+    #  bar = { :a=>1, :b=>2, :c=>3 }
+    #  foo = bar.rekey(:a=>:d,:c=>:a,:d=>:c)
+    #  foo[:a].assert == 3
+    #  foo[:b].assert == 2
+    #  foo[:c].assert == 1
+    #  foo[:d].assert == nil
+    #end
+
     test "with block" do
       bar = { :a=>1, :b=>2 }
       foo = bar.rekey{ |k| k.to_s }
@@ -26,6 +47,25 @@ test_case Hash do
       foo[:a].assert  == nil
       foo[:b].assert  == nil
       foo.assert == { 'a'=>1, 'b'=>2 }
+    end
+
+    test "with block of key, value" do
+      bar = { :a=>1, :b=>2 }
+      foo = bar.rekey{ |k, v| v.to_s }
+      foo['1'].assert == 1
+      foo['2'].assert == 2
+      foo[:a].assert  == nil
+      foo[:b].assert  == nil
+      foo['a'].assert  == nil
+      foo['b'].assert  == nil
+      foo.assert == { '1'=>1, '2'=>2 }
+    end
+
+    test "with keys and block" do
+      expect ArgumentError do
+        bar = { :a=>1, :b=>2 }
+        bar.rekey(:a=>:c){ |k| k.to_s }
+      end
     end
 
     test "symbol proc" do
