432262df7a93cbf85e8a0d5d607541be07546a28
trans <transfire@gmail.com>
2014-01-06 13:43:11 -0500

    Range#to_r became #to_rng.

--- lib/core/facets/range.rb
+++ lib/core/facets/range.rb
@@ -1,9 +1,7 @@
-require 'facets/kernel/require_relative'
-
 require_relative 'range/combine.rb'
 require_relative 'range/overlap.rb'
 #require_relative 'range/quantile.rb'    # uncommon
-require_relative 'range/to_r.rb'
+require_relative 'range/to_rng.rb'
 require_relative 'range/umbrella.rb'
 require_relative 'range/within.rb'
 
