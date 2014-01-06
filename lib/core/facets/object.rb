5a51faa503379456b1ccd5373408f3826acd2f46
trans <transfire@gmail.com>
2014-01-06 12:31:22 -0500

    Object#replace was renamed to #instance_replace.

--- lib/core/facets/object.rb
+++ lib/core/facets/object.rb
@@ -1,8 +1,6 @@
-require 'facets/kernel/require_relative'
-
 require_relative 'object/clone.rb'
 require_relative 'object/dup.rb'
 require_relative 'object/object_state.rb'
-require_relative 'object/replace.rb'
+require_relative 'object/instance_replace.rb'
 require_relative 'object/try_dup.rb'
 
