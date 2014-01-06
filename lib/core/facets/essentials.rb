a7f5db14b5158df49d9c47b48f5d62dcae737773
trans <transfire@gmail.com>
2014-01-06 14:35:59 -0500

    Update essentials.rb for 3.0 changes.

--- lib/core/facets/essentials.rb
+++ lib/core/facets/essentials.rb
@@ -1,8 +1,16 @@
 # As suggested by Roger Pack this script is a simple convenience script
-# for loading only the more "essential" of Facet's core extensions.
+# for loading only the most *essential* of Facets core extensions.
 
-require 'facets/to_hash'
-require 'facets/file/write'
-require 'facets/hash/rekey'
-require 'facets/string/tab'
+# TODO: Work in progress. Please suggest which facets you think belong here.
+
+require_relative 'functor'
+
+#require 'binding/self'
+
+require_relative 'kernel/true'
+require_relative 'kernel/false'
+
+require_relative 'file/write'
+require_relative 'hash/rekey'
+require_relative 'string/indent'
 
