750b8c5fb72e5212de4610735e719296af40d6bb
cielavenir <cielartisan@gmail.com>
2014-02-09 02:58:27 +0900

    fix doc

--- lib/core/facets/array/squeeze.rb
+++ lib/core/facets/array/squeeze.rb
@@ -1,7 +1,6 @@
 class Array
 
-  # Apply a block to array, and recursively apply that block
-  # to each sub-array or +types+.
+  # Destructive version of Enumerable#squeeze.
   #
   #   a = [1,2,2,3,3,2,1]
   #   a.squeeze!
