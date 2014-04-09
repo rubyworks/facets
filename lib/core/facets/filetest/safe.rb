d74abdf36187f49c2073d098b4ad7b9c89e8dff9
trans <transfire@gmail.com>
2014-04-09 09:54:13 -0400

    Unindent FileTest#safe code. [tweak]

--- lib/core/facets/filetest/safe.rb
+++ lib/core/facets/filetest/safe.rb
@@ -4,17 +4,21 @@ module FileTest
 
   module_function
 
-    # Is a path considered reasonably "safe"?
-    #
-    # Do not mistake this for a perfect solution!
-    def safe?(path)
-      case path
-      when /\A(#{SEPARATOR_PATTERN}|\~)(#{SEPARATOR_PATTERN}|\*)+/
-        false
-      else
-        true
-      end
+  # Is a path considered reasonably "safe"?
+  #
+  # Do not mistake this for a perfect solution!
+  # Please help improve if you know how!
+  #
+  # Returns [Boolean]
+
+  def safe?(path)
+    case path
+    when /\A(#{SEPARATOR_PATTERN}|\~)(#{SEPARATOR_PATTERN}|\*)+/
+      false
+    else
+      true
     end
+  end
 
 end
 
