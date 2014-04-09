330dcf502a54dfb4d5ccbf0662b1c821ac5feda5
trans <transfire@gmail.com>
2014-04-09 09:55:24 -0400

    Kernel#deep_copy does not depend on deep_clone.

--- lib/core/facets/kernel/deep_clone.rb
+++ lib/core/facets/kernel/deep_clone.rb
@@ -1,1 +1,27 @@
-require 'facets/kernel/deep_copy'
+module Kernel
+
+  # Deep clone an object by deep cloning every instance
+  # variable as well.
+  #
+  # Returns [Object]
+
+  def deep_clone(cache={})
+    return cache[self] if cache.key?(self)
+
+    copy = clone()
+    cache[self] = copy
+
+    copy.instance_variables.each do |var|
+      val = instance_variable_get(var)
+      begin
+        val = val.deep_clone(cache)
+      rescue TypeError
+        next
+      end
+      copy.instance_variable_set(var, val)
+    end
+
+    return copy
+  end
+
+end
