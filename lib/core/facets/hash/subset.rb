25d30b1590ec366d608fbbd3423ea20faca802e6
trans <transfire@gmail.com>
2014-04-12 14:47:48 -0400

    Hash#subset can take multiple keys.

--- lib/core/facets/hash/subset.rb
+++ lib/core/facets/hash/subset.rb
@@ -4,21 +4,24 @@ class Hash
   # that evaluates to true for each hash key.
   #
   #   {'a'=>1, 'b'=>2}.subset('a')            #=> {'a'=>1}
-  #   {'a'=>1, 'b'=>2}.subset{|k| k == 'a' }  #=> {'a'=>1}
+  #   {'a'=>1, 'b'=>2}.subset{|k| k == 'a'}   #=> {'a'=>1}
   #
   # CREDIT: Alexey Petrushin
-  def subset(keys=nil, &block)
+  def subset(*keys, &block)
     h = {}
-    if keys
-      self.each do |k, v|
-        h[k] = v if keys.include?(k)
-      end
-    else
-      self.each do |k, v|
+
+    if block
+      raise ArgumentError, "subset arguments and block are exclusive parameters" unless keys.empty?
+      each do |k, v|
         h[k] = v if block.call(k)
       end
+    else
+      each do |k, v|
+        h[k] = v if keys.include?(k)
+      end
     end
-    h
+
+    return h
   end
 
 end
