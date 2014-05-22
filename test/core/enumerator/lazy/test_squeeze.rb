1b7ada2de24197dedd063edfb28c5765075e2148
cielavenir <cielartisan@gmail.com>
2014-05-22 16:54:43 +0900

    Add Enumerator::Lazy#squeeze

---
+++ test/core/enumerator/lazy/test_squeeze.rb
@@ -0,0 +1,28 @@
+if defined?(Enumerator::Lazy)
+  covers 'facets/enumerator/lazy/squeeze'
+
+  test_case Enumerator::Lazy do
+
+    method :squeeze do
+
+      # make an Enumerable class to test
+      eC = Class.new do
+        include Enumerable
+        def initialize(*a)
+          @a = a
+        end
+        def each(&b)
+          @a.each(&b)
+        end
+      end
+
+      test 'example' do
+        e = eC.new(1,2,2,3,3,2,1)
+        e.lazy.squeeze.to_a.assert == [1,2,3,2,1]
+      end
+
+    end
+
+  end
+
+end
