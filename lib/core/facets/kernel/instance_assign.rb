f6c25ef2bf042ec2d45a2f643df116cef30b5b5c
trans <transfire@gmail.com>
2014-05-23 09:41:50 -0400

    Add safe option to Kernel#instance_assign and move #instance_replace to from object to kernel directory.w

--- lib/core/facets/kernel/instance_assign.rb
+++ lib/core/facets/kernel/instance_assign.rb
@@ -2,16 +2,25 @@ module Kernel
 
   # Set instance variables using a hash.
   #
-  #   instance_assign('@a'=>1, '@b'=>2)
-  #   @a   #=> 1
-  #   @b   #=> 2
+  #     instance_assign('@a'=>1, '@b'=>2)
+  #     @a   #=> 1
+  #     @b   #=> 2
   #
-  # DEPRECATE: Use instance.update instead of #instance_assign.
-
-  def instance_assign(hash)
-    hash.each do |k,v|
-      k = "@#{k}" if k !~ /^@/
-      instance_variable_set(k, v)
+  # NOTE: Conceptually a better alternative to this method is `instance.update`
+  #       from the `instance` gem. However, pratically this method is probably 
+  #       the better choice until such time that Ruby support annonymous delgators.
+  #
+  def instance_assign(hash, safe=false)
+    if safe
+      hash.each do |k,v|
+        k = "@#{k}" if k !~ /^@/
+        instance_variable_set(k, v) unless instance_variable_defined?(k)
+      end
+    else
+      hash.each do |k,v|
+        k = "@#{k}" if k !~ /^@/
+        instance_variable_set(k, v)
+      end
     end
     return self
   end
---
+++ lib/core/facets/kernel/instance_replace.rb
@@ -0,0 +1,40 @@
+class Object
+
+  # Replace state of object with the state of another object of the
+  # same class (or superclass).
+  #
+  #     class ReplaceExample
+  #       attr_reader :a, :b
+  #       def initialize(a,b)
+  #         @a, @b = a, b
+  #       end
+  #     end
+  #
+  #     obj1 = ReplaceExample.new(1,2)
+  #     obj1.a  #=> 1
+  #     obj1.b  #=> 2
+  #
+  #     obj2 = ReplaceExample.new(3,4)
+  #     obj2.a  #=> 3
+  #     obj2.b  #=> 4
+  #
+  #     obj1.instance_replace(obj2)
+  #     obj1.a  #=> 3
+  #     obj1.b  #=> 4
+  #
+  # This is very similar to `instance.assign`, but it is limited
+  # by the class of object, in the same manner as Array#replace.
+  #
+  # NOTE: Conceptually a better alternative is `instance.replace` provided
+  #       by the `instance` gem. However, pratically this method is probably 
+  #       the better choice until such time that Ruby support annonymous delgators.
+  #
+  def instance_replace(source)
+    raise ArgumentError, "not a #{self.class} -- #{source}" unless source.is_a?(self.class)
+    instance_variables.each do |iv|
+      instance_variable_set(iv, source.instance_variable_get(iv))
+    end
+  end
+
+end
+
