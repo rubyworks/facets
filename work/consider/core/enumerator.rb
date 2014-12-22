7d9a95a03aaa94b94818414e7c52be9a26747d65
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-12-22 15:53:27 +0800

    Fix typo

--- work/consider/core/enumerator.rb
+++ work/consider/core/enumerator.rb
@@ -1,7 +1,7 @@
 
 require 'enumerator'
 
-# This could be used in stead of Enumerator#x. It's more
+# This could be used instead of Enumerator#x. It's more
 # concise, but is it safe?
 
 class Enumerable::Enumerator
