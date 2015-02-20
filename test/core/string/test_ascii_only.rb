7945d202a345283fc1cf4f1816e19a2b8fc6c097
scott m parrish <anithri@gmail.com>
2015-02-20 15:45:37 -0700

    Add demo and extra tests for String#ascii_only :test:

--- test/core/string/test_ascii_only.rb
+++ test/core/string/test_ascii_only.rb
@@ -10,6 +10,14 @@ test_case String do
     test 'with non-ascii' do
      '中文123'.ascii_only.assert == '123'
     end
+
+    test 'with non-ascii when passed alt' do
+      'abc'.ascii_only('!').assert == 'abc'
+    end
+
+    test 'with non-ascii when passed alt' do
+      '中文123'.ascii_only('!').assert == '!!123'
+    end
   end
 
   method :ascii_only! do
@@ -24,6 +32,19 @@ test_case String do
       s.ascii_only!
       s.assert == '123'
     end
+
+    test 'without any non-ascii' do
+      s = 'abc'
+      s.ascii_only!('!')
+      s.assert == 'abc'
+    end
+
+    test 'with non-ascii' do
+      s = '中文123'
+      s.ascii_only!('!')
+      s.assert == '!!123'
+    end
+
   end
 
 end
