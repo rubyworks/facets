7968d4fb404238789ec0d2d5be101138c6ffd211
scott m parrish <anithri@gmail.com>
2015-03-06 12:01:13 -0700

    expand tests for Array#divide. :test: :bug:

--- test/core/array/test_divide.rb
+++ test/core/array/test_divide.rb
@@ -9,6 +9,16 @@ test_case Array do
       r.assert == [['a1','b1'],['a2','b2']]
     end
 
+    test do
+      r = ['a1','b1','a2','b2'].divide(/^b/)
+      r.assert == [['a1'],['b1', 'a2'],['b2']]
+    end
+
+    test do
+      r = ['a1','b1','a2','b2'].divide(/c/)
+      r.assert = [['a1','b1','a2','b2']]
+    end
+
   end
 
 end
