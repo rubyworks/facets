1ff08f978c6b87815fcbe0e52c95562bd70ed605
Ryan Duryea <aguynamedryan@gmail.com>
2015-06-09 20:06:51 -0700

    Remove superfluous comments

--- lib/standard/facets/net/http.rb
+++ lib/standard/facets/net/http.rb
@@ -6,10 +6,6 @@ module Net
   def self.download(url, limit = nil)
     limit ||= 10
     raise ArgumentError, 'HTTP redirect too deep' if limit.zero?
-    #url = URI.parse(url)
-    #req = Net::HTTP::Get.new(url.path)
-    #req['User-Agent'] = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.7) Gecko/2009030422 Ubuntu/8.10 (intrepid) Firefox/3.0.7"
-    #resp = Net::HTTP.new(url.host, url.port).start { |http| http.request(req) }
     resp = Net::HTTP.get_response(URI.parse(url))
     case resp
     when Net::HTTPSuccess     then resp
