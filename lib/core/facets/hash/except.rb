1843a2a138bbdf21a85f58cfc56544f404af9f73
trans <transfire@gmail.com>
2016-05-09 11:36:05 -0400

    Fix Hash#except! to not return rejected entries.
    Also, added Hash#remove! for those who still need old behavior.

--- lib/core/facets/hash/except.rb
+++ lib/core/facets/hash/except.rb
@@ -8,6 +8,18 @@ class Hash
   end
 
   # Replaces hash with new hash less the given keys.
+  #
+  #   h = {:a=>1, :b=>2, :c=>3}
+  #   h.except!(:a)  #=> {:b=>2,:c=>3}
+  #   h              #=> {:b=>2,:c=>3}
+  #
+  # Returns the hash.
+  def except!(*rejected)
+    rejected.each{ |k| delete(k) }
+    self
+  end
+
+  # Replaces hash with new hash less the given keys.
   # This returns the hash of keys removed.
   #
   #   h = {:a=>1, :b=>2, :c=>3}
@@ -15,7 +27,7 @@ class Hash
   #   h              #=> {:b=>2,:c=>3}
   #
   # Returns a Hash of the removed pairs.
-  def except!(*rejected)
+  def remove!(*rejected)
     removed = {}
     rejected.each{ |k| removed[k] = delete(k) }
     removed
--- test/core/hash/test_expect.rb
+++ test/core/hash/test_expect.rb
@@ -15,7 +15,13 @@ test_case Hash do
 
     test do
       h = {:a=>1, :b=>2, :c=>3}
-      h.except!(:a).assert == {:a=>1}
+      h.except!(:a).assert == {:b=>2,:c=>3}
+      h.assert == {:b=>2,:c=>3}
+    end
+
+    test do
+      h = {:a=>1, :b=>2, :c=>3}
+      h.remove!(:a).assert == {:a=>1}
       h.assert == {:b=>2,:c=>3}
     end
 
