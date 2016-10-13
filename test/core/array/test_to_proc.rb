11a0eff422edf62cb514030bf422d5abf87559a9
Tõnis Simo <anton.estum@gmail.com>
2016-10-13 19:12:46 +0300

    Fix wrong tests

--- test/core/array/test_to_proc.rb
+++ test/core/array/test_to_proc.rb
@@ -1,14 +1,14 @@
-covers 'facets/array/only.rb'
+covers 'facets/array/to_proc.rb'
 
 test_case Array do
 
-  method :only do
-
+  method :to_proc do
     test do
-      [5].only.assert == 5
-      [nil].only.assert == nil
-      expect(IndexError){ [].only }
-      expect(IndexError){ [1,2,3].only }
+      the_hash = { one: "One", two: "Two", three: 3, four: nil }
+      the_hash.select(&[[:[], 1], [:is_a?, String]]).
+        assert == { :one => "One", :two => "Two" }
+      the_hash.values.map(&[:to_s, :downcase, [:sub, /one|two|$^/, { "one" => "1", "two" => "2", "" => "0" }]]).
+        assert == ["1", "2", "3", "0"]
     end
 
   end
