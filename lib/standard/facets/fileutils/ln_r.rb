9210ef0d556617c9ecdc42b428f63a73cbd0d2a3
Leif Ringstad <leif@symbolia.com>
2017-09-12 15:38:18 +0200

    Remove deprecated method

--- lib/standard/facets/fileutils/ln_r.rb
+++ lib/standard/facets/fileutils/ln_r.rb
@@ -27,7 +27,6 @@ module FileUtils
     #
     # TODO: Why --remove-destination and not just --force?
     def ln_r(src, dest, options = {})
-      fu_check_options options, OPT_TABLE['ln_r']
       fu_output_message "ln -r#{options[:remove_destination] ? ' --remove-destination' : ''} #{[src,dest].flatten.join ' '}" if options[:verbose]
       return if options[:noop]
       options = options.dup
