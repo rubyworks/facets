cc03746973a925b47fc52f58382d5f13508d84e0
chocolateboy <chocolate@cpan.org>
2018-08-06 23:03:04 +0000

    Remove misplaced example

--- lib/core/facets/array/average.rb
+++ lib/core/facets/array/average.rb
@@ -7,7 +7,6 @@ class Array
   #   [].average #=> nil
   #   [1, 2, 3].average #=> 2
   #   [3, 12, 57, 85, 15, 89, 33, 7, 22, 54].average #=> 37.7
-  #   [1,[2,nil,[3]],nil,4].collapse  #=> [1,2,3,4]
   #
   def average
     return nil if empty?
