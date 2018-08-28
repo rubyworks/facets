ce2df149228d083fd99d8d21a80b4708255d101e
Judith Meyer <yutian.mei@gmail.com>
2018-08-28 21:38:21 +0200

    Basic tests for words_without_punctuation

--- test/core/string/test_words.rb
+++ test/core/string/test_words.rb
@@ -20,5 +20,46 @@ test_case String do
     end
 
   end
+  
+  method :words_without_punctuation do
+    
+    test do
+      # English
+      x = "How?? I don't believe you!!!"
+      x.words_without_punctuation.assert == ['How', 'I', "don't", 'believe', 'you']
+      x = 'Slowly, grudgingly he said: "This has to stop."'
+      x.words_without_punctuation.assert == ['Slowly', 'grudgingly', 'he', 'said', 'This', 'has', 'to', 'stop']
+    end
+    
+    test do
+      # French
+      x = "« Bonjour ! J'ai rendezvous avec mademoiselle Dupont-Fleury ! »"
+      x.words_without_punctuation.assert == ['Bonjour', "J'ai", "rendezvous", "avec", "mademoiselle", "Dupont-Fleury"]
+    end
+    
+    test do
+      # Spanish
+      x = "«¡María, te amo!», exclamó Juan. … «¿Por qué me sigues mintiendo?"
+      x.words_without_punctuation.assert == ['María', 'te', 'amo', 'exclamó', 'Juan', 'Por', 'qué', 'me', 'sigues', 'mintiendo']
+    end
+    
+    test do
+      # Italian
+      x = 'Alcune persone scrivono al computer; altre con la penna: io con le due.'
+      x.words_without_punctuation.assert == ['Alcune', 'persone', 'scrivono', 'al', 'computer', 'altre', 'con', 'la', 'penna', 'io', 'con', 'le', 'due']
+    end
+    
+    test do
+      # German
+      x = '“chevron,” „französische“ Anführungszeichen'
+      x.words_without_punctuation.assert == ['chevron', 'französische', 'Anführungszeichen']
+    end
+    
+    test do
+      # Russian
+      x = '"А ты прав." — сказал он, — "Я великолепен!".'
+      x.words_without_punctuation.assert == ['А', 'ты', 'прав', 'сказал', 'он', 'Я', 'великолепен']
+    end
+  end
 
 end
